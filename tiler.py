2fa5fbe5971019c006194d2f93f5fc9c62c656fc
James Dolezal <jamesmdolezal@gmail.com>
2019-01-12 09:38:30 -0600

    started tiler.py, a GUI to help determine tile size for histology images

---
+++ tiler.py
@@ -0,0 +1,7 @@
+# Tiler v0.1
+# James Dolezal, 2019
+# Application designed to help determine which manigifcation-level and pixel size to use
+#  for tiling of whole-slide histology images
+
+import PyQt5 as pyqt
+
