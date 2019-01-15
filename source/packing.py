97b7384a6746b069a751cc6768a22697e72a05a0
James <jamesmdolezal@gmail.com>
2019-01-14 18:04:47 -0600

    Finished updating new packing module
    Former-commit-id: 253d71bfba080299fa55960af04f37ef052eeffb

--- source/packing.py
+++ source/packing.py
@@ -1,14 +1,3 @@
-# Copyright (C) James Dolezal - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by James Dolezal <jamesmdolezal@gmail.com>, October 2017, Updated 12/16/18
-# ==========================================================================
-"""For a given whole-slide image (WSI), subdivides the image into smaller tiles 
-  using an annotated *.json file generated with LabelMe.
-
-James M Dolezal
-"""
-
 import json
 import os, sys
 import warnings
@@ -20,215 +9,211 @@ from matplotlib import path
 from PIL import Image
 from os.path import isfile, join
 
-# TODO: Progress bar for subdividing/saving stage
-# TODO: Need to filter out normal image boxes that are all white
 
+Image.MAX_IMAGE_PIXELS = None
 SIZE = 256
-FACTOR = 1 # Annotation compression factor, to multiply annotation coordinates
-ROOT_FOLDER = "/Users/James/thyroid/images"
-AN_F = join(ROOT_FOLDER, "Annotated") # Folder with annotated .json files
-SAVE_FOLDER = join(ROOT_FOLDER, str(SIZE))
-CASES = [f[:-5] for f in os.listdir(AN_F) if (isfile(join(AN_F, f)) 
-										and f[-4:] == 'json')]
+FACTOR = 1
+DIRECTORY = "/home/shawarma/thyroid/images/Annotated"
 
-def place_squares(area, offset_x = 0, offset_y = 0, gca = None, graph = False, 
-					image = None, label = None, num = None, exclusions = None, 
-					tile_size = SIZE, color='g', max_squares = None):
-	"""Fill annotation area with squares.
+class Packer:
+	"""Module which loads a LabelMe json annotation file and subdivides annotations
+	into smaller tiles in an optimal fashion.
 
-	Args:
-		area:	*
-		offset_x: *
-		offset_y: *
-		gca: *
-		graph: *
-		image: *
-		label: *
-		num: *
-		exclusions: *
-		tile_size: *
-		color: *
-		max_squares: *
-
-	Returns:
-		Number of total squares (or "tiles") placed
+	Example use:
+		p = Packer(json_data, case_id, 1, 256, "/home/full_image.jpg")
+		p.subdivide_all_annotations()
+		p.export_all_tiles("/home/export_directory")
+		p.show_graph()
 	"""
-	x_min = min(p[0] for p in area) + offset_x
-	x_max = max(p[0] for p in area) + offset_x
-	x_range = x_max - x_min
-	y_min = min(p[1] for p in area) + offset_y
-	y_max = max(p[1] for p in area) + offset_y
-	y_range = y_max - y_min
 
-	square_count = 0
+	def __init__(self, case_data, case_id, factor, size, full_image):
+		"""Initializes module by opening *.json and starting MatPlotLib graph
+		Args:
+			case_data:	LabelMe json data
+			case_id:	Unique number identifying the case
+			factor:		Factor by which annotation file coordinates are compressed (e.g. 10)
+			size:		Size of tiles to generate
+			full_image:	Location of full image from which to extract tiles
+		"""
+		self.data = case_data['shapes']
+		self.case_id = case_id
+		self.factor = factor
+		self.size = size
+		self.background_shape = None # Shape which designates bounding box for the background
+		self.other_shapes = [] # Collection of all non-background shapes
+		self.full_image = full_image
 
-	areaPoly = sg.Polygon(area)
+		plt.axes(label="Packing Result")
+		self.gca = plt.gca()
+		self.gca.invert_yaxis()
+		self.gca.tick_params(axis="x", top=True, labeltop=True, bottom = False, 
+								labelbottom = False)
 
-	# Place test squares
-	for j in range(int(y_range/tile_size)):
-		for i in range(int(x_range/tile_size)):
-			x = x_min + i*tile_size
-			y = y_min + j*tile_size
-			sTestSquare = sg.Polygon([(x, y), (x, y+tile_size), 
-									  (x+tile_size, y+tile_size),
-									  (x+tile_size, y)])
+	def subdivide_all_annotations(self, background_label = "case", ignore_background = True):
+		"""For all annotated shapes, find the starting tile placement which maximizes
+		the number of tiles placed.
 
-			if graph: mTestSquare = plt.Rectangle((x, y), tile_size, tile_size, 
-													fc=color)
+		Args:
+			background_label:	Label in the json file which designates the bounding background shape
+			ignore_background:	If true, will not extract any image tiles from the background shape
+		"""
+		for index, shape in enumerate(self.data):
+			area = shape['points']
+			area_full = np.multiply(area, self.factor)
+			area_small = np.divide(area, self.factor)
+			tile_screening_size = int(SIZE/(FACTOR**2))
 
-			# Check to see if square falls within the current annotation polygon
-			if areaPoly.contains(sTestSquare): 
-				if exclusions:
-					in_area = False
-					for ex in exclusions:
-						if sg.Polygon(ex['points']).intersects(sTestSquare):
-							in_area = True
-							break
-					if in_area: continue
-				if graph: gca.add_patch(mTestSquare) #mTestSquare.set_color('g')
-				if image:
-					if max_squares:
-						sys.stdout.write("\rSubdividing annotation #%s: " % ann_num)
-						progress.bar(square_count, max_squares)
-					region = image.crop((x, y, x+tile_size, y+tile_size))
-					folder = join(SAVE_FOLDER, label)
-					if not os.path.exists(folder): os.makedirs(folder)
+			label = shape['label']
 
-					region.save(join(folder, "%s_%s_%s_0.jpg" % (label, num, square_count)), "jpeg")
-					region.transpose(Image.FLIP_LEFT_RIGHT).save(join(folder, "%s_%s_%s_0F.jpg" % (label, num, square_count)), "jpeg")
-					region.transpose(Image.ROTATE_90).save(join(folder, "%s_%s_%s_90.jpg" % (label, num, square_count)), "jpeg")
-					region.transpose(Image.ROTATE_90).transpose(Image.FLIP_LEFT_RIGHT).save(join(folder, "%s_%s_%s_90F.jpg" % (label, num, square_count)), "jpeg")
-					region.transpose(Image.ROTATE_180).save(join(folder, "%s_%s_%s_180.jpg" % (label, num, square_count)), "jpeg")
-					region.transpose(Image.ROTATE_180).transpose(Image.FLIP_LEFT_RIGHT).save(join(folder, "%s_%s_%s_180F.jpg" % (label, num, square_count)), "jpeg")
-					region.transpose(Image.ROTATE_270).save(join(folder, "%s_%s_%s_270.jpg" % (label, num, square_count)), "jpeg")
-					region.transpose(Image.ROTATE_270).transpose(Image.FLIP_LEFT_RIGHT).save(join(folder, "%s_%s_%s_270F.jpg" % (label, num, square_count)), "jpeg")
-				square_count += 1
-	return square_count
+			if label == background_label and not ignore_background:
+				mPolygon = plt.Polygon(area_full, facecolor="none", edgecolor="r")
+				self.gca.add_patch(mPolygon)
+				self.background_shape = shape
+			elif label != background_label:
+				mPolygon = plt.Polygon(area_full)
+				self.other_shapes.append(shape)
+				squares, coordinates = self.tile_iterator(area_small, index, tile_screening_size)
+				print("Number of tiles: %i" % squares)
+				if squares > 0:
+					print ("Optimal starting point: (%s, %s)" %
+							(coordinates[0] * self.factor, coordinates[1] * self.factor))
+				self.data[index]['square_offset'] = coordinates
+				self.data[index]['max_squares'] = squares
 
-def square_iterator(gca, area, ann_num, tile_size, exclusions=None):
-	"""Iterate through possible square placement starting points and 
-	   find the placement which maximizes number of squares placed."""
+	def export_all_tiles(self, export_dir):
+		"""For all annotated shapes, exports extracted tiles from a full-size image."""
 
-	max_squares = 0
-	max_coord = []
+		im = Image.open(self.full_image)
 
-	max_it = tile_size*tile_size
+		print('Opened %s image from %s | %s %s' % (im.format, self.full_image, 
+													im.size, im.mode))
 
-	for j in range(tile_size):
-		for i in range(tile_size):
-			sys.stdout.write("\rAnalyzing annotation #%s: " % ann_num)
-			progress.bar((j*tile_size)+i, max_it, newline=False)
-			count = place_squares(area, i, j, gca, exclusions = exclusions, 
-									tile_size = tile_size)
-			if count >= max_squares: 
-				max_squares = count
-				max_coord = [i, j]
-	sys.stdout.write("\n")
+		for index, shape in enumerate(self.data):
+			# Increase annotation coordinates by factor
+			area_reduced = shape['points']
+			area = np.multiply(area_reduced, self.factor)
 
-	# Show final result
-	#place_squares(area, max_coord[0], max_coord[1], gca, True)
+			try:
+				offset = shape['square_offset']
+				max_squares = shape['max_squares']
+			except KeyError:
+				print("No annotation found for shape %s, skipping" % index)
+				continue
 
-	return max_squares, max_coord
-
-def subdivider(area, label, offset, im):
-	"""Based on a pre-calculated optimal starting point, subdivide
-	    area into squares."""
-	x_min = min(p[0] for p in area) + offset[0]
-	x_max = max(p[0] for p in area) + offset[1]
-	x_range = x_max - x_min
-	y_min = min(p[1] for p in area) + offset[0]
-	y_max = max(p[1] for p in area) + offset[1]
-	y_range = y_max - y_min
-
-	for j in range(int(y_range)/SIZE):
-		for i in range(int(x_range)/SIZE):
-			x = x_
-
-if __name__==("__main__"):
-	for CASE_ID in CASES:
-		print("Working on case %s" % CASE_ID)
-		with open (join(AN_F, "%s.json" % CASE_ID)) as case:
-			d = json.load(case)
-
-			# Initiate grid for drawing
-			plt.axes(label=CASE_ID)
-			gca = plt.gca()
-			gca.invert_yaxis()
-			gca.tick_params(axis="x", top=True, labeltop=True, bottom = False, 
-							labelbottom = False)
-
-			# Iterate through each annotation
-			ann_num = 1
-			for s in d['shapes']:
-				area = s['points']
-				area_full = np.multiply(area, FACTOR)
-				area_small = np.divide(area, FACTOR)
-
-				label = s['label']
-
-				if label == "case":
-					mPolygon = plt.Polygon(area_full, facecolor="none", edgecolor="r")
-				else: 
-					mPolygon = plt.Polygon(area_full)
-
-				# Add annotation polygon to the grid
-				gca.add_patch(mPolygon)		
-
-				if label == "case": continue
-
-				#squares, sq_co = square_iterator(gca, area, ann_num, int(SIZE/FACTOR))
-				squares, sq_co = square_iterator(gca, area_small, ann_num, 
-													int(SIZE/(FACTOR*FACTOR)))
-
-				print("Number of sections: %i" % squares)
-				if squares > 0: print("Starting point: (%s, %s)\n" % 
-										(sq_co[0]*FACTOR, sq_co[1]*FACTOR))
-				else: print("\n")
-
-				s['square_offset'] = sq_co
-				s['number'] = ann_num
-				s['max_squares'] = squares
-
-				ann_num += 1
-
-			# Now open the image
 			with warnings.catch_warnings():
 				warnings.simplefilter("ignore")
-				im = Image.open((join(AN_F, "%s.jpg" % CASE_ID)))
-			print('Opened %s image "%s.jpg" %s %s' % (im.format, CASE_ID, 
-														im.size, im.mode))
+				self.place_tiles(area, offset, self.size, export_dir, im, index, max_squares, True)
 
-			case_shape = None
-			other_shapes = []
+			sys.stdout.write("\n")
 
-			# Subdivide annotations into squares
-			for s in d['shapes']:
-				label = s['label']
+	def tile_iterator(self, area, _id, tile_size, exclusions = None):
+		"""Iterate through possible tile placement starting points and 
+		find the placement which maximizes number of tiles placed."""
 
-				if label == "case":
-					case_shape = s
-					continue
-				else:
-					other_shapes.append(s)
+		max_squares = 0
+		max_coord = []
 
-				area_reduced = s['points']
-				area = np.multiply(area_reduced, FACTOR)
-				offset = s['square_offset']
-				ann_num = s['number']
-				max_squares = s['max_squares']
+		max_it = tile_size*tile_size
 
-				with warnings.catch_warnings():
-					warnings.simplefilter("ignore")
-					place_squares(area, offset[0], offset[1], gca, graph=True, 
-									image=im, label=CASE_ID, num=ann_num, 
-									max_squares = max_squares)
+		for j in range(tile_size):
+			for i in range(tile_size):
+				sys.stdout.write("\rAnalyzing annotation #%s: " % _id)
+				progress.bar((j*tile_size)+i, max_it, newline=False)
+				count = self.place_tiles(area, [i, j], tile_size, exclusions = exclusions)
+				if count >= max_squares: 
+					max_squares = count
+					max_coord = [i, j]
 
-				sys.stdout.write("\n")
+		sys.stdout.write("\n")
 
-			# Subdivide normal background
-			#print('Subdividing normal background...')
-			#place_squares(case_shape['points'], 0, 0, gca, graph=True, image=im, label='normal', num=0, exclusions = other_shapes, color='r')
+		return max_squares, max_coord
 
-			# Show grid
-			#plt.axis('scaled')
-			#plt.show()
+	def place_tiles(self, area, offset, tile_size, export_dir = None, image = None, _id = None, max_squares = None, graph = False, exclusions = None, color='g'):
+		"""Fills a given area with squares and returns the total number of squares/tiles placed.
+		If an image is provided, will export extracted tiles.
+
+		Args:
+			area:			Bounding box shape, extracted from LabelMe json, in which to place tiles
+			offset:			Coordinates for which to offset tile placement
+			tile_size:		Pixel size of tiles to be placed
+			export_dir:		Directory in which to save  tiles, if extraction from whole image is performed.
+			image: 			Full-size image from which to extract tiles.
+			_id: 			ID of the annotation being tiled
+			max_squares:	Maximum number of tiles that can be placed, if already determined.
+			graph:			Whether or not to display tiling results
+			exclusions:		Array of shapes to exclude from tiling
+			color:			Color code for displaying tile placement, e.g. 'r', 'g'
+
+		Returns:
+			Total number of tiles placed.
+		"""
+
+		x_min = min(p[0] for p in area) + offset[0]
+		x_max = max(p[0] for p in area) + offset[0]
+		x_range = x_max - x_min
+
+		y_min = min(p[1] for p in area) + offset[1]
+		y_max = max(p[1] for p in area) + offset[1]
+		y_range = y_max - y_min
+
+		square_count = 0
+
+		areaPoly = sg.Polygon(area)
+
+		for j in range(int(y_range/tile_size)):
+			for i in range(int(x_range/tile_size)):
+				x = x_min + i*tile_size
+				y = y_min + j*tile_size
+				sTestSquare = sg.Polygon([(x, y), (x, y+tile_size), 
+										  (x+tile_size, y+tile_size),
+										  (x+tile_size, y)])
+
+				if graph: mTestSquare = plt.Rectangle((x, y), tile_size, tile_size, 
+														fc=color)
+
+				# Check to see if square falls within the current annotation polygon
+				if areaPoly.contains(sTestSquare): 
+					if exclusions:
+						in_area = False
+						for ex in exclusions:
+							if sg.Polygon(ex['points']).intersects(sTestSquare):
+								in_area = True
+								break
+						if in_area: continue
+					if graph: self.gca.add_patch(mTestSquare)
+					if image:
+						if max_squares:
+							print("Subdividing annotation #%s: " % _id)
+
+						region = image.crop((x, y, x+tile_size, y+tile_size))
+						region.save(join(export_dir, "%s_%s_%s_0.jpg" % (self.case_id, _id, square_count)), "jpeg")
+						region.transpose(Image.FLIP_LEFT_RIGHT).save(join(export_dir, "%s_%s_%s_0F.jpg" % (self.case_id, _id, square_count)), "jpeg")
+						region.transpose(Image.ROTATE_90).save(join(export_dir, "%s_%s_%s_90.jpg" % (self.case_id, _id, square_count)), "jpeg")
+						region.transpose(Image.ROTATE_90).transpose(Image.FLIP_LEFT_RIGHT).save(join(export_dir, "%s_%s_%s_90F.jpg" % (self.case_id, _id, square_count)), "jpeg")
+						region.transpose(Image.ROTATE_180).save(join(export_dir, "%s_%s_%s_180.jpg" % (self.case_id, _id, square_count)), "jpeg")
+						region.transpose(Image.ROTATE_180).transpose(Image.FLIP_LEFT_RIGHT).save(join(export_dir, "%s_%s_%s_180F.jpg" % (self.case_id, _id, square_count)), "jpeg")
+						region.transpose(Image.ROTATE_270).save(join(export_dir, "%s_%s_%s_270.jpg" % (self.case_id, _id, square_count)), "jpeg")
+						region.transpose(Image.ROTATE_270).transpose(Image.FLIP_LEFT_RIGHT).save(join(export_dir, "%s_%s_%s_270F.jpg" % (self.case_id, _id, square_count)), "jpeg")
+					square_count += 1
+		return square_count
+
+	def show_graph(self):
+		plt.axis('scaled')
+		plt.show()
+
+if __name__==('__main__'):
+	cases = [f[:-5] for f in os.listdir(DIRECTORY) if (isfile(join(DIRECTORY, f)) 
+										and f[-4:] == 'json')]
+
+	for case_id in cases:
+		with open (join(DIRECTORY, "%s.json" % case_id)) as case:
+			full_image = join(DIRECTORY, "%s.jpg" % case_id)
+			export_dir = join(DIRECTORY, str(SIZE), case_id)
+			if not os.path.exists(export_dir): os.makedirs(export_dir)
+
+			case_data = json.load(case)
+
+			packer = Packer(case_data, case_id, FACTOR, SIZE, full_image)
+			packer.subdivide_all_annotations()
+			packer.export_all_tiles(export_dir)
+			packer.show_graph()
