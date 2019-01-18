14af80ff90c67776e3529e10d21dd9fb15e50e40
James Dolezal <jamesmdolezal@gmail.com>
2019-01-18 08:28:58 -0600

    Improvements and documentation to histcon_retrainy
    Former-commit-id: 91503be1d387719340750cb29c8f6a20a88fe29f

--- thyroid/histcon_retrain.py
+++ thyroid/histcon_retrain.py
@@ -28,39 +28,45 @@ import sys
 parser = histcon.parser
 
 RETRAIN_MODEL = '/home/shawarma/thyroid/models/inception_v4_2018_04_27/inception_v4.pb'
+DTYPE = tf.float16 if histcon.FLAGS.use_fp16 else tf.float32
 
-def train():
-	'''Train HISTCON for a number of steps.'''
-
+def retrain():
+	# Do not import the final layer of the saved network, as we will be working with 
+	#  different output classes
 	variables_to_ignore = ("InceptionV4/Logits/Logits/weights:0", "InceptionV4/Logits/Logits/biases:0")
 
 	with tf.Graph().as_default():
+		# Import the graph from the saved *.rb file
 		with gfile.GFile(RETRAIN_MODEL, 'rb') as f:
 			graph_def = tf.GraphDef()
 			graph_def.ParseFromString(f.read())
 		tf.import_graph_def(graph_def, name='')
 
-		with tf.device('/cpu'):
-			images, labels = histcon.processed_inputs()
+		input_placeholder = tf.placeholder(DTYPE, shape=[None, histcon.IMAGE_SIZE, histcon.IMAGE_SIZE, 3])
 
 		imported_variable_dict = {}
 
+		# Create a second, trainable inception_v4 network
 		with arg_scope(inception_arg_scope()):
 			with tf.variable_scope('NewModel'):
-				inception_v4.inception_v4(images, num_classes=histcon.NUM_CLASSES, create_aux_logits=False)		
+				inception_v4.inception_v4(input_placeholder, num_classes=histcon.NUM_CLASSES, create_aux_logits=False)		
 
-			print("Importing saved values...")
-
+			# Start a session to extract the imported tensor values
 			with tf.Session() as sess:
+				# First, get a list of trainable variables from our newly created inception_v4 network
 				for trainable_var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='NewModel'):
 					variable_name = trainable_var.name[9:]
 					if variable_name in variables_to_ignore:
 						continue
+					# For each trainable variable, put the saved value from our imported graph
+					#  into a python dictionary for later use
 					imported_value = tf.get_default_graph().get_tensor_by_name(variable_name).eval(session=sess)
 					imported_variable_dict[variable_name] = imported_value
 
+	# Reset the graph to clear all tensors, including those from our imported graph
 	tf.reset_default_graph()
 
+	# Start a new graph and build a new inception_v4 network
 	with tf.Graph().as_default():
 		global_step = tf.train.get_or_create_global_step()
 
@@ -72,6 +78,7 @@ def train():
 
 			assign_ops = []
 
+			# For each trainable variable, create an op that will assign to it the value we saved
 			for trainable_var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
 				var_name = trainable_var.name
 				if variable_name in variables_to_ignore:
@@ -79,6 +86,7 @@ def train():
 				assign_op = trainable_var.assign(imported_variable_dict[var_name])
 				assign_ops.append(assign_op)
 
+		# Run the ops which will execute the variable data transfer
 		with tf.Session() as sess:
 			sess.run(assign_ops)
 
@@ -126,7 +134,7 @@ def main(argv=None):
 	if tf.gfile.Exists(FLAGS.model_dir):
 		tf.gfile.DeleteRecursively(FLAGS.model_dir)
 	tf.gfile.MakeDirs(FLAGS.model_dir)
-	train()
+	retrain()
 
 if __name__ == "__main__":
 	FLAGS = parser.parse_args()
