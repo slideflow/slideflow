e4b043e1902e91885e802bd76d5d3c63b4705f22
James <jamesmdolezal@gmail.com>
2019-02-21 18:25:08 -0600

    Added nconvert utility script for batch processing
    Former-commit-id: b7f8fe31f1acf6aa4d2558bf97d32aa2eef73374

---
+++ thyroid/nconvert_util.py
@@ -0,0 +1,17 @@
+import os
+from os.path import isfile, join
+import subprocess
+
+root_folder = "/home/shawarma/thyroid/images/normal_background/2048"
+resize_name = "512_from_2048"
+resize_folder = join(root_folder, resize_name)
+if not os.path.isdir(resize_folder): 
+	os.mkdir(resize_folder)
+
+cases = [f for f in os.listdir(root_folder) if (not isfile(join(root_folder, f)) and f != resize_name)]
+for case in cases:
+	print("Converting images from case {}".format(case))
+	os.mkdir(join(resize_folder, case))
+	command = 'nconvert -out jpeg -o {}/%.jpg -resize 25% 25% {}/*.jpg'.format(join(resize_folder, case), join(root_folder, case))
+	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	
