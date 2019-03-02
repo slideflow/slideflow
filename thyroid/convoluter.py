53203bf49f15ce72ffb34b9c726edec960948bf1
James Dolezal <jamesmdolezal@gmail.com>
2019-03-02 14:53:34 -0600

    Merged convoluter.py and conv_display.py
    Former-commit-id: 1a2621d84d22343c104bd0dc8d1384ba39032230

--- thyroid/convoluter.py
+++ thyroid/convoluter.py
@@ -4,6 +4,8 @@
 # Written by James Dolezal <jamesmdolezal@gmail.com>, October 2017, Updated 12/16/18
 # ==========================================================================
 
+'''Convolutionally applies a saved Tensorflow model to a larger image, displaying
+the result as a heatmap overlay.'''
 
 from __future__ import absolute_import
 from __future__ import division
@@ -26,7 +28,16 @@ from inception_utils import inception_arg_scope
 import conv_display
 import progress_bar
 
+from multiprocessing import Pool
+from matplotlib.widgets import Slider
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.cm as cm
+import matplotlib.colors as mcol
+
 # NOTE!! THIS MODULE IS CURRENTLY NOT WORKING, THERE IS A BUG WITH WINDOWING
+# Not currently working; overlay is offset
+# Also not efficient; an alternative solution using multiprocessing has been half implemented here
 
 class Convoluter:
     # Model variables
@@ -259,7 +270,40 @@ class Convoluter:
                 coord.request_stop()
                 coord.join(threads, stop_grace_period_secs=10)
                 print("\nFinished.")
-                conv_display.display(filename, output, self.SIZE, self.STRIDES)
+                self.display(filename, output, self.SIZE, self.STRIDES)
+
+    def display(image_file, logits, size, stride):
+        print("Received logits, size=%s, stride=%sx%s (%s x %s)" % (size, stride[1], stride[2], len(logits), len(logits[0])))
+        print("Calculating overlay matrix...")
+
+        im = plt.imread(image_file)
+        implot = plt.imshow(im,zorder=0)
+        gca = plt.gca()
+        gca.tick_params(axis="x", top=True, labeltop=True, bottom=False, labelbottom=False)
+
+        im_extent = implot.get_extent()
+        extent = [im_extent[0] + size/2, im_extent[1] - size/2, im_extent[2] - size/2, im_extent[3] + size/2]
+
+        # Define color map
+        jetMap = np.linspace(0.45, 0.95, 255)
+        cmMap = cm.nipy_spectral(jetMap)
+        newMap = mcol.ListedColormap(cmMap)
+
+        sl = logits[:, :, 1]
+        
+        # Consider alternate interpolations: none, bicubic, quadric, lanczos
+        heatmap = plt.imshow(sl, extent=extent, cmap=newMap, alpha = 0.3, interpolation='bicubic', zorder=10)
+
+        def update_opacity(val):
+            heatmap.set_alpha(val)
+
+        # Show sliders to adjust heatmap overlay
+        ax_opac = plt.axes([0.25, 0.05, 0.5, 0.03], facecolor='lightgoldenrodyellow')
+        opac = Slider(ax_opac, 'Opacity', 0, 1, valinit = 1)
+        opac.on_changed(update_opacity)
+
+        plt.axis('scaled')
+        plt.show()
 
 def main(argv=None):
     c = Convoluter()
