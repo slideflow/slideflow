992770d4700ca92b3f716d407f7103bbdeca40ed
James <jamesmdolezal@gmail.com>
2019-03-02 23:17:09 -0600

    Further progress on new convoluter, some assembly still required
    Former-commit-id: 7211b723509e060038b0e67cb723fd1bba262fa9

--- thyroid/new_convoluter.py
+++ thyroid/new_convoluter.py
@@ -14,8 +14,19 @@ from __future__ import print_function
 import sys
 import warnings
 
-from PIL import Image
+import progress_bar
+
 import tensorflow as tf
+import numpy as np
+import inception_v4
+from tensorflow.contrib.framework import arg_scope
+from inception_utils import inception_arg_scope
+from PIL import Image
+
+from matplotlib.widgets import Slider
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as mcol
 
 # TODO: extract_image_patches expands data too unnecessarily, creating large memory pressure.
 #  Need to find iterator method to only extract as many patches as are needed to generate a batch
@@ -23,8 +34,8 @@ import tensorflow as tf
 class Convoluter:
 	# Model variables
 	SIZE = 512
-	NUM_CLASSES = 1
-	BATCH_SIZE = 32
+	NUM_CLASSES = 5
+	BATCH_SIZE = 512
 	USE_FP16 = True
 
 	# Display variables
@@ -32,7 +43,8 @@ class Convoluter:
 	STRIDES = [1, int(SIZE/stride_divisor), int(SIZE/stride_divisor), 1]
 	WINDOW_SIZE = 6000
 
-	WHOLE_IMAGE = '/Users/james/thyroid/images/234781-2.jpg'
+	WHOLE_IMAGE = '/home/shawarma/thyroid/images/WSI_25/234794-1_25.jpg'
+	MODEL_DIR = '/home/shawarma/thyroid/models/active' # Directory where to write event logs and checkpoints.
 
 	def __init__(self):
 		self.DTYPE = tf.float16 if self.USE_FP16 else tf.float32
@@ -61,6 +73,9 @@ class Convoluter:
 
 				coord = []
 
+				self.X_SIZE = shape[0] - window_size[0]
+				self.Y_SIZE = shape[1] - window_size[1]
+
 				for x in range(0, (shape[0]+1) - window_size[0], window_stride[0]):
 					for y in range(0, (shape[1]+1) - window_size[1], window_stride[1]):
 						coord.append([x, y])
@@ -70,25 +85,86 @@ class Convoluter:
 				iterator = coord_dataset.make_one_shot_iterator()
 				next_coord = iterator.get_next()
 
-				next_windows = tf.map_fn(lambda c: image[c[0]:c[0]+window_size[0], c[1]:c[1]+window_size[1],], tf.cast(next_coord, dtype=tf.int32))
-				#next_windows = tf.map_fn(tf.image.per_image_standardization, tf.cast(next_window, tf.float32))
+				batch = tf.map_fn(lambda c: image[c[0]:c[0]+window_size[0], c[1]:c[1]+window_size[1],], tf.cast(next_coord, dtype=tf.int32))
+				batch = tf.map_fn(lambda patch: tf.cast(tf.image.per_image_standardization(patch), self.DTYPE), batch, dtype=self.DTYPE)
 
-				next_jpegs = tf.map_fn(lambda c: tf.image.encode_jpeg(tf.cast(c, tf.uint8)), next_windows, dtype=tf.string)
+			batch.set_shape([self.BATCH_SIZE, self.SIZE, self.SIZE, 3])
 
-				x = 0
-				while 1:
+			with arg_scope(inception_arg_scope()):
+				_, end_points = inception_v4.inception_v4(batch, num_classes = self.NUM_CLASSES)
+
+			slogits = end_points['Predictions']
+			saver = tf.train.Saver()
+
+			with tf.Session() as sess:
+				init = (tf.global_variables_initializer(), tf.local_variables_initializer())
+				sess.run(init)
+
+				ckpt = tf.train.get_checkpoint_state(self.MODEL_DIR)
+				if ckpt and ckpt.model_checkpoint_path:
+					print("Restoring saved checkpoint model.")
+					saver.restore(sess, ckpt.model_checkpoint_path)
+				else:
+					raise Exception('Unable to find checkpoint file.')
+
+				logits_arr = []
+
+				x_logits_len = int(self.X_SIZE / window_stride[0])
+				y_logits_len = int(self.Y_SIZE / window_stride[1])
+				total_logits_count = x_logits_len * y_logits_len
+
+				count = 0
+
+				while True:
 					try:
-						jpegs = sess.run(next_jpegs)
-						for j in range(len(jpegs)):
-							print("Writing image {} from batch {}".format(j, x))
-							write_op = tf.write_file('/Users/james/thyroid/images/sample{}-{}.jpg'.format(x, j), jpegs[j])
-							sess.run(write_op)
-
+						progress_bar.bar(count, total_logits_count, text = "Calculated {} images out of {}. ".format(count, total_logits_count))
+						logits_arr.append(sess.run(tf.cast(slogits, tf.float32)))#, feed_dict={batch_pl: batch}))
 					except tf.errors.OutOfRangeError:
+						print("End of image detected.")
 						break
-					x += 1
+					count += self.BATCH_SIZE
+				progress_bar.end()
+				
+			logits_out = np.resize(logits_arr, [x_logits_len, y_logits_len, self.NUM_CLASSES])
+			self.display(self.WHOLE_IMAGE, logits_out, self.SIZE)
 
-			sys.exit()
+	def display(self, image_file, logits, size):
+		'''Displays logits calculated using scan_image as a heatmap overlay.'''
+		print("Received logits, size=%s, (%s x %s)" % (size, len(logits), len(logits[0])))
+		print("Calculating overlay matrix...")
+
+		im = plt.imread(image_file)
+		implot = plt.imshow(im,zorder=0)
+		gca = plt.gca()
+		gca.tick_params(axis="x", top=True, labeltop=True, bottom=False, labelbottom=False)
+
+		im_extent = implot.get_extent()
+		extent = [im_extent[0] + size/2, im_extent[1] - size/2, im_extent[2] - size/2, im_extent[3] + size/2]
+
+		# Define color map
+		jetMap = np.linspace(0.45, 0.95, 255)
+		cmMap = cm.nipy_spectral(jetMap)
+		newMap = mcol.ListedColormap(cmMap)
+
+		#print(logits)
+
+		sl = logits[:, :, 4]
+
+		print(sl)
+
+		# Consider alternate interpolations: none, bicubic, quadric, lanczos
+		heatmap = plt.imshow(sl, extent=extent, cmap=newMap, alpha = 0.3, interpolation='bicubic', zorder=10)
+
+		def update_opacity(val):
+			heatmap.set_alpha(val)
+
+		# Show sliders to adjust heatmap overlay
+		ax_opac = plt.axes([0.25, 0.05, 0.5, 0.03], facecolor='lightgoldenrodyellow')
+		opac = Slider(ax_opac, 'Opacity', 0, 1, valinit = 1)
+		opac.on_changed(update_opacity)
+
+		plt.axis('scaled')
+		plt.show()
 
 if __name__==('__main__'):
 	c = Convoluter()
