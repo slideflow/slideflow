0095139e0a6afe10997cc7b0164a9268fc28fded
James <jamesmdolezal@gmail.com>
2019-03-26 18:55:19 -0500

    Added multithreading to convolution and a new progress bar
    Former-commit-id: e3335d226cfa1ad5133597f22313a4c8a0be2bf3

--- source/progress_bar.py
+++ source/progress_bar.py
@@ -2,6 +2,63 @@
 # James M Dolezal, 2018
 
 import sys
+import time
+
+class ProgressBar:
+	def __init__(self, bar_length=20):
+		self.BARS = {}
+		self.bar_length = bar_length
+		self.next_id = 0
+
+	def add_bar(self, val, endval, endtext=''):
+		bar_id = self.next_id
+		self.next_id += 1
+
+		class Bar:
+			value = val
+			endvalue = endval
+			text = endtext
+			id = bar_id
+			def percent(self):
+				return float(self.value) / self.endvalue
+
+		self.BARS.update({bar_id: Bar()})
+		self.refresh()
+		return bar_id
+
+	def arrow(self, percent):
+		return chr(0x2588) * int(round(percent * self.bar_length))
+
+	def update(self, _id, val, text=None):
+		self.BARS[_id].value = val
+		if text:
+			self.BARS[_id].text = text
+		self.refresh()
+
+	def refresh(self):
+		out_text = "\r\033[K"
+		for bar_id in self.BARS:
+			bar = self.BARS[bar_id]
+			arrow = self.arrow(bar.percent())
+			spaces = u'-' * (self.bar_length - len(arrow))
+			out_text += u"\u007c{0}\u007c {1}% ({2})   ||   ".format(arrow + spaces, int(round(bar.percent() * 100)), bar.text)
+		sys.stdout.write(out_text)
+		sys.stdout.flush()
+
+	def end(self, _id):
+		if not _id:
+			for bar in self.BARS:
+				bar.value = bar.endvalue
+			self.refresh()
+			sys.stdout.write('\n')
+		else:
+			del self.BARS[_id]
+			self.refresh()
+
+	def print(self, text):
+		sys.stdout.write("\r" + text + "\033[K\n")
+		sys.stdout.flush()
+		self.refresh()
 
 def bar(value, endvalue, bar_length=20, newline=True, text=''):
 	percent = float(value) / endvalue
@@ -10,7 +67,7 @@ def bar(value, endvalue, bar_length=20, newline=True, text=''):
 
 	if newline:
 		sys.stdout.write("\r")
-    	
+
 	sys.stdout.write(u"\u007c{0}\u007c {1}% {2}".format(arrow + spaces, int(round(percent * 100)), text))
 	sys.stdout.flush()
 
@@ -18,3 +75,18 @@ def end():
 	bar(10, 10)
 	sys.stdout.flush()
 	print('\n')
+
+if __name__==('__main__'):
+	p = ProgressBar()
+	id1 = p.add_bar(0,100, "James")
+	id2 = p.add_bar(0,500, "Christopher")
+	for i in range(300):
+		time.sleep(0.1)
+		p.update(id1, (i/30)*10)
+		if i == 15:
+			p.print("Hello there!")
+		if i == 30:
+			p.print("How are you?")
+		else:
+			p.update(id2, (i/10)*50)
+	p.end()
