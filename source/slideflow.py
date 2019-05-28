1ba8e2b97afc8b2e42a5f62c827fad8f4a0707df
James Dolezal <jamesmdolezal@gmail.com>
2019-05-28 17:17:37 -0500

    Streamlined helper script output through initiation of training

--- source/slideflow.py
+++ source/slideflow.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 import json
 import shutil
 import tensorflow as tf
@@ -9,6 +10,7 @@ from pathlib import Path
 from glob import glob
 
 import convoluter
+import histcon
 from  util import datasets, tfrecords
 
 class SlideFlowProject:
@@ -19,6 +21,7 @@ class SlideFlowProject:
 	ROI_DIR = None
 	TILES_DIR = None
 	MODELS_DIR = None
+	PRETRAIN_DIR = None
 	USE_TFRECORD = False
 	TFRECORD_DIR = None
 	DELETE_TILES = False
@@ -58,6 +61,13 @@ class SlideFlowProject:
 			self.generate_tfrecord()
 
 	def extract_tiles(self):
+		if self.get_task('extract_tiles') == 'complete':
+			print('Tile extraction already complete.')
+			return
+		elif self.get_task('extract_tiles') == 'in process':
+			print('Tile extraction already in process; restart? [y/N] ')
+			sys.exit()
+		print("Extracting image tiles...")
 		self.update_task('extract_tiles', 'in process')
 		convoluter.NUM_THREADS = self.NUM_THREADS
 		if not exists(join(self.TILES_DIR, "train_data")):
@@ -68,13 +78,19 @@ class SlideFlowProject:
 		c = convoluter.Convoluter(self.TILE_PX, self.TILE_UM, self.NUM_CLASSES, self.BATCH_SIZE, 
 									self.USE_FP16, join(self.TILES_DIR, "train_data"), self.ROI_DIR, self.AUGMENTATION)
 
-		slide_list = glob(join(self.SLIDES_DIR, '**/*.svs'))
-		slide_list.extend(glob(join(self.SLIDES_DIR, '**/*.jpg')))
+		slide_list = glob(join(self.SLIDES_DIR, '**!(thumbs)/*.svs'))
+		slide_list.extend(glob(join(self.SLIDES_DIR, '*.svs')))
+		slide_list.extend(glob(join(self.SLIDES_DIR, '**!(thumbs)/*.jpg')))
+		slide_list.extend(glob(join(self.SLIDES_DIR, '*.jpg')))
 		c.load_slides(slide_list)
 		c.convolute_slides(export_tiles=True)
 		self.update_task('extract_tiles', 'complete')
 	
 	def separate_training_and_eval(self):
+		if self.get_task('separate_training_and_eval') == 'complete':
+			print('Training and eval dataset separation already complete.')
+			return
+		print('Separating training and eval datasets...')
 		self.update_task('separate_training_and_eval', 'in process')
 		datasets.build_validation(join(self.TILES_DIR, "train_data"), join(self.TILES_DIR, "eval_data"), fraction = self.EVAL_FRACTION)
 		self.update_task('separate_training_and_eval', 'complete')
@@ -83,6 +99,10 @@ class SlideFlowProject:
 		# Note: this will not work as the write_tfrecords function expects a category directory
 		# Will leave as is to manually test performance with category defined in the TFRecrod
 		#  vs. dynamically assigning category via annotation metadata during training
+		if self.get_task('generate_tfrecord') == 'complete':
+			print('TFRecords already generated.')
+			return
+		print('Writing TFRecord files...')
 		self.update_task('generate_tfrecord', 'in process')
 		tfrecords.write_tfrecords(join(self.TILES_DIR, "train_data"), self.TFRECORD_DIR, "train")
 		tfrecords.write_tfrecords(join(self.TILES_DIR, "eval_data"), self.TFRECORD_DIR, "eval")
@@ -90,6 +110,20 @@ class SlideFlowProject:
 			shutil.rmtree(join(self.TILES_DIR, "train_data"))
 			shutil.rmtree(join(self.TILES_DIR, "eval_data"))
 		self.update_task('generate_tfrecord', 'complete')
+
+	def start_training(self, model_name):
+		self.update_task('generate_tfrecord', 'in process')
+		print(f"Training model {model_name}...")
+		model_dir = join(self.MODELS_DIR, model_name)
+		tensorboard_dir = join(model_dir, 'logs/projector')
+		if not exists(model_dir):
+			datasets.make_dir(model_dir)
+		if not exists(tensorboard_dir):
+			datasets.make_dir(tensorboard_dir)
+		input_dir = self.TFRECORD_DIR if self.USE_TFRECORD else self.TILES_DIR
+		histcon_model = histcon.HistconModel(model_dir, input_dir)
+		histcon_model.train(restore_checkpoint = self.PRETRAIN_DIR)
+		os.system(f'tensorboard --logdir={tensorboard_dir}')
 
 	def create_global_path(self, path_string):
 		if path_string and (len(path_string) > 2) and path_string[:2] == "./":
@@ -167,6 +201,9 @@ class SlideFlowProject:
 		data['tasks'][task] = status
 		self.write_config(data, self.CONFIG)
 
+	def get_task(self, task):
+		return self.parse_config(self.CONFIG)['tasks'][task]
+
 	def load_project(self):
 		if os.path.exists(self.CONFIG):
 			data = self.parse_config(self.CONFIG)
@@ -177,6 +214,7 @@ class SlideFlowProject:
 			self.ROI_DIR = data['ROI'] 
 			self.TILES_DIR = data['tiles']
 			self.MODELS_DIR = data['models']
+			self.PRETRAIN_DIR = data['pretraining']
 			self.TILE_UM = data['tile_um']
 			self.TILE_PX = data['tile_px']
 			self.NUM_CLASSES = data['num_classes']
@@ -208,8 +246,10 @@ class SlideFlowProject:
 			self.TFRECORD_DIR = self.dir_input("Where should the TFRecord files be stored? (recommend HDD) [./tfrecord] ",
 									default='./tfrecord', create_on_invalid=True)
 		# Training
-		self.MODELS_DIR = self.dir_input("Where are the saved models stored? [./models] ",
+		self.MODELS_DIR = self.dir_input("Where should the saved models be stored? [./models] ",
 									default='./models', create_on_invalid=True)
+		if self.yes_no_input("Will models utilize pre-training? [y/N] ", default=False):
+			self.PRETRAIN_DIR = self.dir_input("Where is the pretrained model folder located? ", create_on_invalid=False)
 		self.TILE_UM = self.int_input("What is the tile width in microns? [280] ", default=280)
 		self.TILE_PX = self.int_input("What is the tile width in pixels? [512] ", default=512)
 		self.NUM_CLASSES = self.int_input("How many classes are there to be trained? ")
@@ -224,6 +264,7 @@ class SlideFlowProject:
 		data['ROI'] = self.ROI_DIR
 		data['tiles'] = self.TILES_DIR
 		data['models'] = self.MODELS_DIR
+		data['pretraining'] = self.PRETRAIN_DIR
 		data['tile_um'] = self.TILE_UM
 		data['tile_px'] = self.TILE_PX
 		data['num_classes'] = self.NUM_CLASSES
--- source/util/datasets.py
+++ source/util/datasets.py
@@ -4,6 +4,7 @@ from os import listdir, makedirs
 from os.path import isfile, isdir, join, exists
 from random import shuffle
 import argparse
+import util.utilities as sfutil
 
 def make_dir(_dir):
 	''' Makes a directory if one does not already exist, in a manner compatible with multithreading. '''
@@ -15,21 +16,17 @@ def make_dir(_dir):
 
 def build_validation(train_dir, eval_dir, fraction = 0.1):
 	make_dir(eval_dir)
-	cat_dirs = [_dir for _dir in listdir(train_dir) if isdir(join(train_dir, _dir))]
-	for cat_dir in cat_dirs:
-		print("Category {}:".format(cat_dir))
-		make_dir(join(eval_dir, cat_dir))
-		case_dirs = [_dir for _dir in listdir(join(train_dir, cat_dir)) if isdir(join(train_dir, cat_dir, _dir))]
-		for case_dir in case_dirs:
-			make_dir(join(eval_dir, cat_dir, case_dir))
-			files = [file for file in listdir(join(train_dir, cat_dir, case_dir)) 
-						if (isfile(join(train_dir, cat_dir, case_dir, file))) and
-						   (file[-3:] == "jpg")]
-			shuffle(files)
-			num_to_move = int(len(files)*fraction)
-			for file in files[0:num_to_move]:
-				shutil.move(join(train_dir, cat_dir, case_dir, file), join(eval_dir, cat_dir, case_dir, file))
-			print("  Moved {} files for case {}".format(num_to_move, case_dir))
+	case_dirs = [_dir for _dir in listdir(train_dir) if isdir(join(train_dir, _dir))]
+	for case_dir in case_dirs:
+		make_dir(join(eval_dir, case_dir))
+		files = [file for file in listdir(join(train_dir, case_dir)) 
+					if (isfile(join(train_dir, case_dir, file))) and
+						(file[-3:] == "jpg")]
+		shuffle(files)
+		num_to_move = int(len(files)*fraction)
+		for file in files[0:num_to_move]:
+			shutil.move(join(train_dir, case_dir, file), join(eval_dir, case_dir, file))
+		print(f" + Set aside {num_to_move} tiles for case {sfutil.green(case_dir)} for eval dataset")
 
 def merge_validation(train_dir, eval_dir):
 	cat_dirs = [_dir for _dir in listdir(eval_dir) if isdir(join(eval_dir, _dir))]
