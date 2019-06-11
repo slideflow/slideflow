fa52974e77479f52f011a62326111847a126c830
James Dolezal <jamesmdolezal@gmail.com>
2019-06-11 17:30:14 -0500

    Training returns validation loss

--- source/inception_v4.py
+++ source/inception_v4.py
@@ -282,7 +282,7 @@ def inception_v4(inputs, num_classes=1001, is_training=True,
   end_points = {}
   with tf.variable_scope(scope, 'InceptionV4', [inputs], reuse=reuse) as scope:
     with slim.arg_scope([slim.batch_norm], is_training=is_training,
-						decay=0.9): #default decay is 0.999
+						decay=0.95): #default decay is 0.999
       with slim.arg_scope([slim.dropout], is_training=is_training):
         net, end_points = inception_v4_base(inputs, scope=scope)
 
