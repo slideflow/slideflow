14041bfe38811dd54b27b74c64a804a59bfe2d5d
James <jamesmdolezal@gmail.com>
2019-06-21 18:10:15 -0500

    New branch for debugging

--- source/run_project.py
+++ source/run_project.py
@@ -7,13 +7,14 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	# here's a comment
-
 	SFP = sf.SlideFlowProject(args.project)
 	#SFP.prepare_tiles()
 	#SFP.extract_tiles(slide_filters = {'dataset': ['train']})
 	#SFP.separate_training_and_eval()
-	SFP.generate_tfrecord()
-	SFP.batch_train()
-	#SFP.evaluate(model="/home/shawarma/data/slideflow_projects/thyroid_5_cat/models/NFCOMP_c/trained_model.h5", dataset='test')
+	#SFP.generate_tfrecord()
+	#SFP.generate_manifest()
+	#SFP.batch_train(resume_training="/home/shawarma/data/slideflow_projects/thyroid_5_cat/models/retrain_balanced2/trained_model.h5")
+	#SFP.evaluate(model="/home/shawarma/data/slideflow_projects/thyroid_5_cat/models/retrain_balanced3/trained_model.h5")
 	#SFP.train_model('model2')
-	#SFP.generate_heatmaps('PT_ModelB', slide_filters={'dataset': ['eval']})
+	SFP.generate_heatmaps('retrain_balanced3', slide_filters={'dataset': ['eval']})
+	
