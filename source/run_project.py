ddd12e2c69c00c78bfc4d57a7ee10b4f1df4c132
James <jamesmdolezal@gmail.com>
2019-06-22 10:44:26 -0500

    bug fixes

--- source/run_project.py
+++ source/run_project.py
@@ -1,4 +1,4 @@
-import slideflow as sf
+amport slideflow as sf
 import argparse
 
 if __name__ == '__main__':
@@ -8,13 +8,12 @@ if __name__ == '__main__':
 
 	# here's a comment
 	SFP = sf.SlideFlowProject(args.project)
-	#SFP.prepare_tiles()
 	#SFP.extract_tiles(slide_filters = {'dataset': ['train']})
 	#SFP.separate_training_and_eval()
 	#SFP.generate_tfrecord()
 	#SFP.generate_manifest()
-	#SFP.batch_train(resume_training="/home/shawarma/data/slideflow_projects/thyroid_5_cat/models/retrain_balanced2/trained_model.h5")
+	SFP.batch_train()#resume_training="/home/shawarma/data/slideflow_projects/thyroid_5_cat/models/retrain_balanced2/trained_model.h5")
 	#SFP.evaluate(model="/home/shawarma/data/slideflow_projects/thyroid_5_cat/models/retrain_balanced3/trained_model.h5")
 	#SFP.train_model('model2')
-	SFP.generate_heatmaps('retrain_balanced3', slide_filters={'dataset': ['eval']})
+	#SFP.generate_heatmaps('retrain_balanced3', slide_filters={'dataset': ['eval']})
 	
