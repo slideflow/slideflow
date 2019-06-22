e38bc413cf72a37052fece7d4715bc3ad14cf867
James Dolezal <jamesmdolezal@gmail.com>
2019-06-22 12:56:39 -0400

    debugging

--- source/util/tfrecords.py
+++ source/util/tfrecords.py
@@ -60,11 +60,19 @@ def _parse_tfrecord_function(record):
 	features = tf.io.parse_single_example(record, FEATURE_DESCRIPTION)
 	return features
 
-def _read_and_return_record(record, assign_case=None, assign_category=None):
+def _read_and_return_features(record):
 	features = _parse_tfrecord_function(record)
-	case = features['case'].numpy() if not assign_case else assign_case
-	category = features['category'].numpy() if not assign_category else assign_category
+	case = features['case'].numpy()
+	category = features['category'].numpy()
 	image_raw = features['image_raw'].numpy()
+	return case, category, image_raw
+
+def _read_and_return_record(record, assign_case=None, assign_category=None):
+	case, category, image_raw = _read_and_return_features(record)
+	if assign_case:
+		case = assign_case
+	if assign_category:
+		category = assign_category
 	tf_example = image_example(category, case, image_raw)
 	return tf_example.SerializeToString()
 
