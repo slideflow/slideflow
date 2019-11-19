59ec3cf2be8997c67cece6d1c5941fe61ee4405c
James Dolezal <jamesmdolezal@gmail.com>
2019-11-19 09:38:02 -0600

    Updated default sample_actions.py for compatibility

--- source/slideflow/sample_actions.py
+++ source/slideflow/sample_actions.py
@@ -2,12 +2,12 @@ def main(SFP):
 	#SFP.create_blank_annotations_file()
 	#SFP.associate_slide_names()
 	
-	#SFP.extract_tiles(filters = {'to_extract': 'yes'}, subfolder='all')
+	#SFP.extract_tiles(filters = {'to_extract': 'yes'})
 		   
 	#SFP.create_hyperparameter_sweep(finetune_epochs=[5], toplayer_epochs=0, model=['Xception'], pooling=['avg'], loss='sparse_categorical_crossentropy', 
 	#								learning_rate=[0.00001, 0.001], batch_size=64, hidden_layers=[1], optimizer='Adam', early_stop=True, early_stop_patience=15, balanced_training=['BALANCE_BY_CATEGORY'],
 	#								balanced_validation='NO_BALANCE', augment=True, filename=None)
-	#SFP.train(subfolder='all',
+	#SFP.train(
 	#	  outcome_header="category",
 	#	  filters = {
 	#		  'dataset': 'train',
@@ -15,7 +15,7 @@ def main(SFP):
 	#	  },
 	#	  batch_file='batch_train.tsv')
 
-	#SFP.evaluate(model='HPSweep0-kfold3', subfolder='all', outcome_header="category", filters = {'dataset': 'eval'})
+	#SFP.evaluate(model='HPSweep0-kfold3', outcome_header="category", filters = {'dataset': 'eval'})
 	#SFP.generate_heatmaps('HPSweep0')
 	#SFP.generate_mosaic('HPSweep0')
 	pass
