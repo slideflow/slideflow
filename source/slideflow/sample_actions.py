ef0ea0bd64e05dc52374c37f8f282cfea3fed13f
James <jamesmdolezal@gmail.com>
2019-12-18 21:00:27 -0600

    Better sample_actions.py with more examples and descriptions

--- source/slideflow/sample_actions.py
+++ source/slideflow/sample_actions.py
@@ -1,9 +1,14 @@
 def main(SFP):
-	#SFP.create_blank_annotations_file()
+	# First run only: automatically associate slide names in the annotations file
+	# ---------------------------------------------------------------------------
 	#SFP.associate_slide_names()
 	
+	# Perform tile extraction
+	# -----------------------
 	#SFP.extract_tiles()
 		   
+	# Train with a hyperparameter sweep
+	# ---------------------------------
 	#SFP.create_hyperparameter_sweep(finetune_epochs=[5], toplayer_epochs=0, model=['Xception'], pooling=['avg'], loss='sparse_categorical_crossentropy', 
 	#								learning_rate=[0.00001, 0.001], batch_size=64, hidden_layers=[1], optimizer='Adam', early_stop=True, early_stop_patience=15, balanced_training=['BALANCE_BY_CATEGORY'],
 	#								balanced_validation='NO_BALANCE', augment=True, filename=None)
@@ -15,20 +20,25 @@ def main(SFP):
 	#	  },
 	#	  batch_file='batch_train.tsv')
 
-	#SFP.evaluate(model='HPSweep0-kfold3', outcome_header="category", filters = {'dataset': 'eval'})
-	#SFP.generate_heatmaps('HPSweep0')
-	#SFP.resize_tfrecords(598)
-	#SFP.generate_mosaic('/home/shawarma/data/slideflow_projects/TCGA_HNSC_HPV_598px_604um/external_models/TCGA_HNSC_HPV_598px_604um/trained_model_epoch5.h5', resolution='high')
+	# Evaluate model performance with separate data
+	# ---------------------------------------------
+	#SFP.evaluate(model='HPSweep0-kfold3', outcome_header="category", filters = {'dataset': ['eval']})
 
+	# Create heatmaps of predictions with a certain model
+	# ---------------------------------------------------
+	#SFP.generate_heatmaps('HPSweep0', filters = {'dataset': ['eval']})
+
+	# Generate a mosaic map of tiles using a certain model
+	# ----------------------------------------------------
+	#SFP.generate_mosaic('/home/shawarma/data/slideflow_projects/TCGA_HNSC_HPV_598px_604um/external_models/TCGA_HNSC_HPV_598px_604um/trained_model_epoch5.h5', 
+	# 						filters = {'dataset': ['eval']}, resolution='high')
+
+	# Visualize and analyze penultimate layer activations
+	# ---------------------------------------------------
 	#AV = SFP.generate_activations_analytics(outcome_header="HPV", filters={"HPV": ["HPV+", "HPV-"]})
 	#AV.generate_box_plots()
-	#umap = AV.plot_2D_umap()
+	#AV.plot_2D_umap()
 	#top_nodes = AV.get_top_nodes_by_slide()
 	#for node in top_nodes[:10]:
 	#	AV.plot_3D_umap(node)
-	#filtered_tiles = AV.filter_tiles_by_umap(umap, x_lower = 0.4,
-	#											   x_upper = 0.45,
-	#											   y_lower = 0.2,
-	#											   y_upper = 0.25)
-	#AV.save_example_tiles_gradient(tile_filter=filtered_tiles)
 	pass
