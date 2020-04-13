d8261d79c4794fbeca383c4c3702656c15d4f863
sarakochanny <skochanny@uchicago.edu>
2020-04-13 18:48:14 -0500

    I added the regularization function 'add_regularization' to util and then called it in model.py. I left some comments in model.py as well for your review.

--- source/slideflow/util/__init__.py
+++ source/slideflow/util/__init__.py
@@ -5,6 +5,7 @@ import time
 import os
 import shutil
 import datetime
+import tempfile
 
 import tensorflow as tf
 
@@ -491,3 +492,31 @@ def update_results_log(results_log_path, model_name, results_dict):
 	# Delete the old results log file
 	if exists(f"{results_log_path}.temp"):
 		os.remove(f"{results_log_path}.temp")
+
+# Added by Sara 4/13/2020
+def add_regularization(model, L2_weight):
+	regularizer=tf.keras.regularizers.l2(L2_weight)
+	# this function is from "https://sthalles.github.io/keras-regularizer/"
+	# adds regularizer to layers where you can set such an attribute, in this case, L2 regularization.
+	if not isinstance(regularizer, tf.keras.regularizers.Regularizer):
+		print("Regularizer must be a subclass of tf.keras.regularizers.Regularizer")
+		return model
+
+	for layer in model.layers:
+		for attr in ['kernel_regularizer']:
+			if hasattr(layer, attr):
+				setattr(layer, attr, regularizer)
+
+        # When we change the layers attributes, the change only happens in the model config file
+	model_json = model.to_json()
+
+	# Save the weights before reloading the model.
+	tmp_weights_path = os.path.join(tempfile.gettempdir(), 'tmp_weights.h5')
+	model.save_weights(tmp_weights_path)
+
+	# load the model from the config
+	model = tf.keras.models.model_from_json(model_json)
+
+	# Reload the model weights
+	model.load_weights(tmp_weights_path, by_name=True)
+	return model
