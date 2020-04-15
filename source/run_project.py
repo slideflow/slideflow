6b88e7efc13cae479ef81ff03984c70d6b2d8aa4
James <jamesmdolezal@gmail.com>
2020-04-15 08:45:18 -0500

    Testing mode, BatchNorm removal
    - Removed testing mode
    - Removed BatchNorm temporary fix

--- source/run_project.py
+++ source/run_project.py
@@ -12,7 +12,6 @@ if __name__=='__main__':
 	parser.add_argument('-g', '--gpu', type=int, default=2, help='Number of available GPUs.')
 	parser.add_argument('-t', '--threads', type=int, default=4, help='Number of threads to use during tile extraction.')
 	parser.add_argument('-sV', '--skip_verification', action="store_true", help="Whether or not to skip verification.")
-	parser.add_argument('-tM', '--test_mode', action="store_true", help="Whether or not to train in test mode.")
 	parser.add_argument('--nfs', action="store_true", help="Sets environmental variable HDF5_USE_FILE_LOCKING='FALSE' as a fix to problems with NFS file systems.")
 	args = parser.parse_args()
 
@@ -23,7 +22,6 @@ if __name__=='__main__':
 	SFP = sf.SlideflowProject(args.project)
 	SFP.autoselect_gpu(args.gpu)
 	SFP.FLAGS['skip_verification'] = args.skip_verification
-	SFP.FLAGS['test_mode'] = args.test_mode
 	SFP.FLAGS['num_threads'] = args.threads
 
 	sys.path.insert(0, args.project)
