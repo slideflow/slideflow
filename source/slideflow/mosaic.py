ee37ffad3c094af74c3ca4f07ebf7dfc2244cbfe
James <jamesmdolezal@gmail.com>
2020-04-17 08:58:06 -0500

    Early work on new mosaic system
    - Separates mosaic functions into separate submodule and increased flexibility

---
+++ source/slideflow/mosaic.py
@@ -0,0 +1,8 @@
+from slideflow.util import log
+
+def load_patient_level_umap_from_csv(csv_file):
+
+class Mosaic:
+	def __init__(self, umap=None, focus=None, leniency=1.5, expanded=False, tile_zoom=15, num_tiles_x=50, resolution='high', 
+					export=True, tile_um=None, use_fp16=True, save_dir=None)):
+		pass
