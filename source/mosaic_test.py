9485f6c7de062887f11bf43f9d3c1673e9287640
James <jamesmdolezal@gmail.com>
2020-04-18 10:49:15 -0500

    Updates to Mosaic, TFRecordUMAP,  ActivationsVisualizer, and Dataset classes
    - Separated mosaic functions in ActivationsVisualizer into separate mosaic.Mosaic class
    - Separated UMAP functions from ActivationsVisualizer into separate statistics.TFRecordUMAP class
    - Streamlined Dataset class with apply_filters function

--- source/mosaic_test.py
+++ source/mosaic_test.py
@@ -1,50 +1,35 @@
 import slideflow as sf
 import numpy as np
 
-from slideflow.activations import ActivationsVisualizer
+from slideflow.mosaic import Mosaic
+from slideflow.statistics import TFRecordUMAP
+from slideflow.io.datasets import Dataset
+from os.path import join
 
 project_folder = '/home/shawarma/data/slideflow_projects/TCGA_HNSC_OP_299px_302um/'
 filters = None
 umap_category = 'type'
-
-SFP = sf.SlideflowProject(project_folder)
-dataset = sf.io.datasets.Dataset(config_file=SFP.PROJECT['dataset_config'], sources=SFP.PROJECT['datasets'])
-dataset.load_annotations(SFP.PROJECT['annotations'])
-tfrecords = dataset.get_filtered_tfrecords(filters)
-dataset_slides = [tfrecord.split('/')[-1][:-10] for tfrecord in tfrecords]
-manifest = dataset.get_manifest()
-
 outcome_headers = ["umap-all-x", "umap-all-y"]
 
-outcomes, _ = dataset.get_outcomes_from_annotations(outcome_headers, filters=filters, 
-												 				  	 filter_blank=outcome_headers,
-																	 use_float=True)
-slides = [slide for slide in list(outcomes.keys()) if slide in dataset_slides]
-
-print("Setup complete.")
+SFP = sf.SlideflowProject(project_folder)
+dataset = Dataset(config_file=SFP.PROJECT['dataset_config'], sources=SFP.PROJECT['datasets'])
+dataset.load_annotations(SFP.PROJECT['annotations'])
+dataset.apply_filters(filters=filters, filter_blank=outcome_headers)
+tfrecords = dataset.get_tfrecords()
+slides = dataset.get_slides()
+outcomes, _ = dataset.get_outcomes_from_annotations(outcome_headers, use_float=True)
 
 umap_x = np.array([outcomes[slide]['outcome'][0] for slide in slides])
 umap_y = np.array([outcomes[slide]['outcome'][1] for slide in slides])
 umap_meta = [{'slide': slide, 'index': 0} for slide in slides]
 
-umap = {
-	'nodes': None,
-	'slides': slides,
-	'umap_x': umap_x,
-	'umap_y': umap_y,
-	'umap_meta': umap_meta
-}
+umap = TFRecordUMAP(tfrecords=dataset.get_tfrecords(), slides=dataset.get_slides())
+umap.load_precalculated(umap_x, umap_y, umap_meta)
 
-AV = ActivationsVisualizer(model=None,
-						   tfrecords=tfrecords, 
-						   root_dir=SFP.PROJECT['root'],
-						   image_size=SFP.PROJECT['tile_px'],
-						   annotations=SFP.PROJECT['annotations'],
-						   outcome_header=umap_category,
-						   focus_nodes=None,
-						   use_fp16=SFP.PROJECT['use_fp16'],
-						   batch_size=32,
-						   export_csv=False)
+mosaic_map = Mosaic(umap, leniency=1.5,
+						  expanded=False,
+						  tile_zoom=15,
+						  num_tiles_x=100,
+						  resolution='high')
 
-AV.generate_mosaic(umap=umap)
-AV.plot_2D_umap(umap=umap)
+mosaic_map.save(join(SFP.PROJECT['root'], 'stats'))
