42412f6492772a59c35b60201cdbb40bcf554db6
James <jamesmdolezal@gmail.com>
2020-09-13 11:41:39 -0500

    Distributed training attempt

--- source/slideflow/gan/__init__.py
+++ source/slideflow/gan/__init__.py
@@ -45,87 +45,89 @@ def gan_test(batch_size=4, use_mixed_precision=False):
 		policy = tf.keras.mixed_precision.experimental.Policy('mixed_float16')
 		mixed_precision.set_policy(policy)
 
-	# Setup project-specific details. This will eventually need to be replaced
-	#  With a more flexibile solution.
-	sample_project = '/home/shawarma/Thyroid-Paper-Final/projects/TCGA'
-	SFP = sf.SlideflowProject(sample_project)
-	sf_dataset = SFP.get_dataset(tile_px=299, tile_um=302, filters={'brs_class': ['Braf-like', 'Ras-like']})
-	tfrecords = sf_dataset.get_tfrecords()
-	xception_path = '/home/shawarma/Thyroid-Paper-Final/projects/TCGA/models/brs-BRS_FULL/trained_model_epoch1.h5'
-	vgg16_path = '/home/shawarma/Thyroid-Paper-Final/projects/TCGA/models/brs-BRS_VGG16_FULL_NEWT/trained_model_epoch1.h5'
+	keras_strategy = tf.distribute.MirroredStrategy()
+	with keras_strategy.scope():
+		# Setup project-specific details. This will eventually need to be replaced
+		#  With a more flexibile solution.
+		sample_project = '/home/shawarma/Thyroid-Paper-Final/projects/TCGA'
+		SFP = sf.SlideflowProject(sample_project)
+		sf_dataset = SFP.get_dataset(tile_px=299, tile_um=302, filters={'brs_class': ['Braf-like', 'Ras-like']})
+		tfrecords = sf_dataset.get_tfrecords()
+		xception_path = '/home/shawarma/Thyroid-Paper-Final/projects/TCGA/models/brs-BRS_FULL/trained_model_epoch1.h5'
+		vgg16_path = '/home/shawarma/Thyroid-Paper-Final/projects/TCGA/models/brs-BRS_VGG16_FULL_NEWT/trained_model_epoch1.h5'
 
-	# Build actual dataset inputs using a slideflow model
-	checkpoint_dir = '/home/shawarma/test_log'
-	slide_annotations, _ = sf_dataset.get_outcomes_from_annotations('brs', use_float=True)
-	train_tfrecords = tfrecords
-	validation_tfrecords = None
-	manifest = sf_dataset.get_manifest()
-	SFM = SlideflowModel(checkpoint_dir, 299, slide_annotations, train_tfrecords, validation_tfrecords, manifest, model_type='linear')
-	dataset, _, num_tiles = SFM._build_dataset_inputs(tfrecords, batch_size, 'NO_BALANCE', augment=False,
-																												 include_slidenames=False,
-																												 parse_fn=partial(_parse_tfrecord_brs, sf_model=SFM,
-																												   									   n_classes=2))
-	dataset = dataset.prefetch(20)
-	
-	# Load the external model
-	with tf.name_scope('ExternalModel'):
-		model = tf.keras.models.load_model(vgg16_path)	
+		# Build actual dataset inputs using a slideflow model
+		checkpoint_dir = '/home/shawarma/test_log'
+		slide_annotations, _ = sf_dataset.get_outcomes_from_annotations('brs', use_float=True)
+		train_tfrecords = tfrecords
+		validation_tfrecords = None
+		manifest = sf_dataset.get_manifest()
+		SFM = SlideflowModel(checkpoint_dir, 299, slide_annotations, train_tfrecords, validation_tfrecords, manifest, model_type='linear')
+		dataset, _, num_tiles = SFM._build_dataset_inputs(tfrecords, batch_size, 'NO_BALANCE', augment=False,
+																													include_slidenames=False,
+																													parse_fn=partial(_parse_tfrecord_brs, sf_model=SFM,
+																																						n_classes=2))
+		dataset = dataset.prefetch(20)
+		
+		# Load the external model
+		with tf.name_scope('ExternalModel'):
+			model = tf.keras.models.load_model(vgg16_path)	
 
-	# Set loaded model as non-trainable
-	for layer in model.layers:
-		layer.trainable = False
+		# Set loaded model as non-trainable
+		for layer in model.layers:
+			layer.trainable = False
 
-	# Identify the feature activation maps that will be used for the generator input
-	feature_tensors = {
-		'image': model.input,
-		'image_vgg16': model.get_layer('vgg16').input,
-		'fc8':   model.get_layer('hidden_1').output,
-		'fc7':   model.get_layer('hidden_0').output,
-		'conv0': model.get_layer('vgg16').get_layer('block5_pool').output, # 512 channels (9x9)
-		'conv1': model.get_layer('vgg16').get_layer('block4_pool').output, # 512 channels (18x18)
-		'conv2': model.get_layer('vgg16').get_layer('block3_pool').output, # 256 channels (37x37)
-		'conv3': model.get_layer('vgg16').get_layer('block2_pool').output, # 128 channels (74x74)
-		'conv4': model.get_layer('vgg16').get_layer('block1_pool').output, # 64 channels  (149x149)
-	}
+		# Identify the feature activation maps that will be used for the generator input
+		feature_tensors = {
+			'image': model.input,
+			'image_vgg16': model.get_layer('vgg16').input,
+			'fc8':   model.get_layer('hidden_1').output,
+			'fc7':   model.get_layer('hidden_0').output,
+			'conv0': model.get_layer('vgg16').get_layer('block5_pool').output, # 512 channels (9x9)
+			'conv1': model.get_layer('vgg16').get_layer('block4_pool').output, # 512 channels (18x18)
+			'conv2': model.get_layer('vgg16').get_layer('block3_pool').output, # 256 channels (37x37)
+			'conv3': model.get_layer('vgg16').get_layer('block2_pool').output, # 128 channels (74x74)
+			'conv4': model.get_layer('vgg16').get_layer('block1_pool').output, # 64 channels  (149x149)
+		}
 
-	# Build the generator and discriminator
-	with tf.name_scope('Generator'):
-		generator, generator_input_layers, mask_sizes, mask_order = semantic_model.create_generator(feature_tensors, n_classes=2, z_dim=128)
+		# Build the generator and discriminator
+		with tf.name_scope('Generator'):
+			generator, generator_input_layers, mask_sizes, mask_order = semantic_model.create_generator(feature_tensors, n_classes=2, z_dim=128)
 
-	with tf.name_scope('Discriminator'):
-		discriminator = semantic_model.create_discriminator(image_size=299)	
+		with tf.name_scope('Discriminator'):
+			discriminator = semantic_model.create_discriminator(image_size=299)	
 
-	# Build a model that will output pooled features from the reference model, to be used for reconstruction loss
-	features_with_pool = [tf.cast(feature_tensors['fc8'], dtype=tf.float32),
-					 	  tf.cast(feature_tensors['fc7'], dtype=tf.float32),
-						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv0']), dtype=tf.float32),
-						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv1']), dtype=tf.float32),
-						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv2']), dtype=tf.float32),
-						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv3']), dtype=tf.float32),
-						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv4']), dtype=tf.float32)
-	]
-	input_layers = [feature_tensors['image'], feature_tensors['image_vgg16']]
-	reference_features = tf.keras.models.Model(input_layers, features_with_pool)
+		# Build a model that will output pooled features from the reference model, to be used for reconstruction loss
+		features_with_pool = [tf.cast(feature_tensors['fc8'], dtype=tf.float32),
+							tf.cast(feature_tensors['fc7'], dtype=tf.float32),
+							tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv0']), dtype=tf.float32),
+							tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv1']), dtype=tf.float32),
+							tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv2']), dtype=tf.float32),
+							tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv3']), dtype=tf.float32),
+							tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv4']), dtype=tf.float32)
+		]
+		input_layers = [feature_tensors['image'], feature_tensors['image_vgg16']]
+		reference_features = tf.keras.models.Model(input_layers, features_with_pool)
 
-	# Setup the dataset which will be supplying the feature masks
-	with tf.name_scope('Masking'):
-		conv_masks = ('mask_conv0', 'mask_conv1', 'mask_conv2', 'mask_conv3', 'mask_conv4')
-		mask_dataset = semantic.mask_dataset(mask_sizes, mask_order=mask_order,
-														 conv_masks=conv_masks,
-														 image_size=299,
-														 batch_size=batch_size)
-	# Print model summaries
-	print("Model summary")
-	model.summary()
-	print("Generator summary")
-	generator.summary()
-	print("Discriminator summary")
-	discriminator.summary()
+		# Setup the dataset which will be supplying the feature masks
+		with tf.name_scope('Masking'):
+			conv_masks = ('mask_conv0', 'mask_conv1', 'mask_conv2', 'mask_conv3', 'mask_conv4')
+			mask_dataset = semantic.mask_dataset(mask_sizes, mask_order=mask_order,
+															conv_masks=conv_masks,
+															image_size=299,
+															batch_size=batch_size)
+		# Print model summaries
+		print("Model summary")
+		model.summary()
+		print("Generator summary")
+		generator.summary()
+		print("Discriminator summary")
+		discriminator.summary()
 
-	# Begin training
-	semantic.train(dataset, generator, discriminator, reference_features, mask_dataset, mask_order=mask_order,
-																	conv_masks=conv_masks,
-																	image_size=299, 
-																	batch_size=batch_size,
-																	z_dim=128,
-																	steps_per_epoch=round(num_tiles/batch_size))
+		# Begin training
+		semantic.train(dataset, generator, discriminator, reference_features, mask_dataset, mask_order=mask_order,
+																		conv_masks=conv_masks,
+																		image_size=299, 
+																		batch_size=batch_size,
+																		z_dim=128,
+																		steps_per_epoch=round(num_tiles/batch_size))
