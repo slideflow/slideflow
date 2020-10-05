2113d51648eb825a6b8489bb92eada29b9e78125
James Dolezal <jdolezal1@MacBook-Pro-9.local>
2020-10-05 06:51:59 -0500

    large generator

--- source/slideflow/gan/semantic/generator.py
+++ source/slideflow/gan/semantic/generator.py
@@ -11,6 +11,8 @@ from slideflow.gan.sagan.spectral_norm_conv import DenseSN #ConvSN2D, ConvSN2DTr
 from slideflow.gan.sagan.spectral_normalization import SpectralNormalization
 #from slideflow.gan.sagan.wzspectral import SNConv2D, SNConv2DTranspose
 
+from slideflow.gan.semantic.model import masked_feature_input
+
 def block(x, labels, out_channels, num_classes):
 	in_channel = x.get_shape().as_list()[-1]
 	x_0 = x
@@ -71,3 +73,50 @@ def create_generator(
 	out = tf.keras.layers.Activation('tanh', dtype=tf.float32)(act6)
 
 	return tf.keras.models.Model([noise_input, class_input], out), [noise_input, class_input], dummy_mask_sizes, mask_order
+
+def create_generator_large(
+	feature_tensors,
+	n_classes,
+	z_dim=128,
+	gf_dim=64,
+	use_alt_block=False
+):
+	'''Creates a generator.'''
+	mask_order = ('mask_conv0', 'mask_conv1', 'mask_conv2', 'mask_conv3', 'mask_conv4')
+	dummy_mask_sizes = {m: (1,) for m in mask_order}
+	resblock = alt_block if use_alt_block else block
+
+	# Inputs
+	noise_input = tf.keras.layers.Input((z_dim,), name='noise_input')
+	class_input = tf.keras.layers.Input(shape=(1,), dtype=tf.int32, name='class_input')
+	c = tf.one_hot(class_input, n_classes, dtype=tf.int32)
+	c = tf.squeeze(c, axis=1)
+	input_layers = [noise_input, class_input, feature_tensors['image'], feature_tensors['image_vgg16']]
+
+	# Feature inputs
+	features_with_pool = [#tf.cast(feature_tensors['fc8'], dtype=tf.float32),
+						  #tf.cast(feature_tensors['fc7'], dtype=tf.float32),
+						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv0']), dtype=tf.float32),
+						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv1']), dtype=tf.float32),
+						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv2']), dtype=tf.float32),
+						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv3']), dtype=tf.float32),
+						  tf.cast(tf.keras.layers.MaxPool2D((2,2))(feature_tensors['conv4']), dtype=tf.float32),
+	]
+
+	# Linear layers
+	act0 = DenseSN(gf_dim * 16 * 4 * 4)(noise_input)
+	act0 = tf.keras.layers.Reshape((4, 4, gf_dim * 16))(act0)
+
+	# Conv layers
+	act1 = resblock(act0, c, gf_dim * 16, n_classes)
+	act2 = resblock(act1, c, gf_dim * 8, n_classes)
+	act3 = resblock(act2, c, gf_dim * 4, n_classes)
+	act3, _ = SelfAttnModel(gf_dim * 4)(act3)
+	act4 = resblock(act3, c, gf_dim * 2 , n_classes)
+	act5 = resblock(act4, c, gf_dim, n_classes)
+	act5 = ConditionalBatchNorm(gf_dim)(act5, c)
+	act5 = tf.keras.layers.LeakyReLU()(act5)
+	act6 = SpectralConv2DTranspose(3, kernel_size=3, strides=1, padding='same')(act5)
+	out = tf.keras.layers.Activation('tanh', dtype=tf.float32)(act6)
+
+	return tf.keras.models.Model(input_layers, out), input_layers, dummy_mask_sizes, mask_order
