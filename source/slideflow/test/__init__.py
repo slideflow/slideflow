09e9d413d532abe620bf7d073b47a588035b6b69
James <jamesmdolezal@gmail.com>
2020-11-03 12:48:40 -0600

    TestSuite automatic data download
    - TestSuite now automatically downloads slides from TCGA if local data is not already present

--- source/slideflow/test/__init__.py
+++ source/slideflow/test/__init__.py
@@ -3,124 +3,148 @@ import slideflow as sf
 import tensorflow as tf
 import csv
 import shutil
+import requests
+import json
+import re
 
+import slideflow.util as sfutil
 from slideflow.io import Dataset
-from slideflow.util import TCGA, log, ProgressBar
+from slideflow.util import log, ProgressBar
 from slideflow.statistics import TFRecordMap
 
 from glob import glob
 from os.path import join
 
-''' Todo:
-- JPG, TIFF, SVS
-- Verify properties: dimensions, properties (dict), level_dimensions, level_count, level_downsamples
-- Verify ROI (area, coordinates)
-- Verify thumbnail
-- Verify extracted tiles at different pixels and microns
-- Verify logits and prelogits based on saved model
-- Verify heatmaps
-'''
+# Todo:
+#- JPG, TIFF, SVS
+#- Verify properties: dimensions, properties (dict), level_dimensions, level_count, level_downsamples
+#- Verify ROI (area, coordinates)
+#- Verify thumbnail
+#- Verify extracted tiles at different pixels and microns
+#- Verify logits and prelogits based on saved model
+#- Verify heatmaps
 
-# TCGA SVS Download
+class TestConfigurator:
+	def __init__(self, root):
+		self.DATASETS = {
+			'TEST': {
+				'slides': 	join(root, 'slides'),
+				'roi': 		join(root, 'roi'),
+				'tiles': 	join(root, 'project', 'tiles', 'TEST'), 
+				'tfrecords':join(root, 'project', 'tfrecords', 'TEST') 
+			},
+			'TMA': {
+				'slides': 	join(root, 'tma_slides'),
+				'roi': 		join(root, 'tma_roi'),
+				'tiles': 	join(root, 'project', 'tiles', 'TMA'),
+				'tfrecords':join(root, 'project', 'tfrecords', 'TMA')
+			}
+		}
+		self.PROJECT = {
+			'root': join(root, 'project'), 
+			'name': 'TEST_PROJECT',
+			'annotations': join(root, 'project', 'annotations.csv'),
+			'dataset_config': join(root, 'datasets.json'),
+			'datasets': ['TEST', 'TMA'],
+			'models_dir': join(root, 'project', 'models'),
+			'tile_um': 302,
+			'tile_px': 299,
+			'use_fp16': True,
+			'batch_train_config': join(root, 'project', 'batch_train.csv'),
+			'validation_fraction': 0.2,
+			'validation_target': 'per-patient',
+			'validation_strategy': 'k-fold',
+			'validation_k_fold': 3,
+		}
+		self.SLIDES = {
+			'TCGA-BJ-A2N9-01Z-00-DX1.CFCB1FA9-7890-4B1B-93AB-4066E160FBF5':'0b0b560d-f3e7-4103-9b1b-d4981e00c0e7',
+			'TCGA-BJ-A3PT-01Z-00-DX1.A307F39F-AE85-42F4-B705-11AF06F391D9':'0eeb9df4-4cb0-4075-9e18-3861dea2ba05',
+			'TCGA-BJ-A45J-01Z-00-DX1.F3646444-749B-4583-A45D-17C580FCB866':'0c376805-5f09-4687-8e29-ad36b2171577',
+			'TCGA-DJ-A2PT-01Z-00-DX1.8C28F7F7-426A-4AAC-8AC6-D082F85C4D34':'1af4e340-38d3-4589-8a7b-6be3f207bc06',
+			'TCGA-DJ-A4UQ-01Z-00-DX1.2F88113C-4F3B-4250-A7C3-5B01AB6ABE55':'0d0e4ddf-749c-44ba-aea9-989732e79d8d',
+			'TCGA-DJ-A13W-01Z-00-DX1.02059A44-7DF1-420D-BA48-587D611F34F5':'0c5592d5-b51c-406a-9dd5-72778e982f13',
+			'TCGA-DO-A1K0-01Z-00-DX1.5ED4011C-6AAA-4197-8044-1F69D55CEAEE':'0d78b583-ecf2-45f4-95a4-dc61057be898',
+			'TCGA-E3-A3E5-01Z-00-DX1.E7E8AB8B-695F-4158-A3C0-E2B801E07D2A':'1a4242c5-495d-46f2-b87d-050acc6cef44',
+			'TCGA-E8-A242-01Z-00-DX1.9DDBB5BB-696E-4C61-BF4A-464062403F04':'1bcfd879-c48b-4232-b6a7-ff1337be9914',
+			'TCGA-EL-A3CO-01Z-00-DX1.7BF5F004-E7E6-4320-BA89-39D05657BBCB':'0ac4f9a9-32f8-40b5-be0e-52ceeef7dbbf',
+			'TCGA-EM-A1YA-01Z-00-DX1.6CEACBCA-9D05-4BF4-BFF2-725F869ABFA8':'0b239138-5e5e-4080-89a3-e19052f4cb7d',
+			'TCGA-EM-A2CN-01Z-00-DX1.8A82A101-19A6-4809-B873-B67014DD9E0A':'1ad7bea7-5279-4c28-9357-b74a52e6b4c0',
+			'TCGA-EM-A3SY-01Z-00-DX1.34A87FD3-A47E-448B-8558-24C4FE15C641':'01bd4e78-0a78-46c8-a4d5-7028ac3c84f8',
+			'TCGA-EM-A22M-01Z-00-DX1.3E4CFB51-A1A5-48B5-96BC-F3EE763C7C5A':'0d6306d5-c687-4d59-a9be-2af18b9a8a2e',
+			'TCGA-H2-A3RH-01Z-00-DX1.444D8191-ADFF-4A59-BC33-B08F246594AE':'0c4a266b-708d-4f01-8b9b-ac6272ffefff',
+			'TCGA-IM-A41Z-01Z-00-DX1.BEB9E1F0-75E7-418D-921A-0B28268F52FE':'1ac4a8aa-f370-4d0d-9a41-6f3eb2f324ca',
+		}
+		self.ANNOTATIONS = [
+			[sfutil.TCGA.patient, 'dataset', 'category1', 'category2', 'linear1', 'linear2'],
+			['TCGA-BJ-A2N9', 'TEST', 'PTC-follicular', 'BRAF', '1.1', '1.2'],
+			['TCGA-BJ-A3PT', 'TEST', 'PTC-follicular', 'BRAF', '2.1', '2.2'],
+			['TCGA-BJ-A45J', 'TEST', 'PTC-follicular', 'BRAF', '2.2', '1.2'],
+			['TCGA-DJ-A2PT', 'TEST', 'PTC-follicular', 'BRAF', '2.8', '4.2'],
+			['TCGA-DJ-A4UQ', 'TEST', 'PTC-follicular', 'Non-mutant', '4.3', '3.2'],
+			['TCGA-DJ-A13W', 'TEST', 'PTC-follicular', 'Non-mutant', '2.2', '1.2'],
+			['TCGA-DO-A1K0', 'TEST', 'PTC-follicular', 'Non-mutant', '0.2', '1.1'],
+			['TCGA-E3-A3E5', 'TEST', 'PTC-follicular', 'Non-mutant', '7.2', '4.2'],
 
-# Case TCGA-EM-A1YB
+			['TCGA-E8-A242', 'TEST', 'NIFTP', 'cat2a', '2.8', '4.8'],
+			['TCGA-EL-A3CO', 'TEST', 'NIFTP', 'cat2b', '2.8', '4.7'],
+			['TCGA-EM-A1YA', 'TEST', 'NIFTP', 'cat2a', '3.8', '4.6'],
+			['TCGA-EM-A2CN', 'TEST', 'NIFTP', 'cat2b', '4.8', '4.5'],
+			['TCGA-EM-A3SY', 'TEST', 'NIFTP', 'cat2a', '5.8', '4.4'],
+			['TCGA-EM-A22M', 'TEST', 'NIFTP', 'cat2b', '6.8', '4.2'],
+			['TCGA-IM-A41Z', 'TEST', 'NIFTP', 'cat2a', '7.8', '4.1'],
 
-# import requests
-# import json
-# import re
-# params = {'ids': ["281ba1d7-69c9-4e0d-a33b-47ad05bada22"]}
-# data_endpt = "https://api.gdc.cancer.gov/data"
-# response = requests.post(data_endpt, data = json.dumps(params), headers = {"Content-Type": "application/json"})
-# response_head_cd = response.headers["Content-Disposition"]
-# file_name = re.findall("filename=(.+)", response_head_cd)[0]
-# with open(file_name, "wb") as output_file:
-#	output_file.write(response.content)
+			['TMA_1185', 'TMA', 'PRAD', 'None', '7.8', '4.1'],
+		]
 
-# --- TEST suite configuration --------------------------------------------------------
-TEST_DATASETS = {
-	'TEST1': {
-		'slides': '/home/shawarma/data/HNSC/train_slides',
-		'roi': '/home/shawarma/data/HNSC/train_slides',
-		'tiles': '/home/shawarma/data/test_project/tiles/TEST1',
-		'tfrecords': '/home/shawarma/data/test_project/tfrecords/TEST1',
-	},
-	'TEST2': {
-		'slides': '/mnt/raid/SLIDES/THCA/UCH',
-		'roi': '/mnt/raid/SLIDES/THCA/UCH',
-		'tiles': '/home/shawarma/data/test_project/tiles/TEST2',
-		'tfrecords': '/home/shawarma/data/test_project/tfrecords/TEST2',
-	},
-	'TMA': {
-		'slides': '/home/shawarma/data/test_slides',
-		'roi': '/home/shawarma/data/test_slides',
-		'tiles': '/home/shawarma/data/test_project/tiles/TMA',
-		'tfrecords': '/home/shawarma/data/test_project/tfrecords/TMA',
-	}
-}
-PROJECT_CONFIG = {
-	'root': '/home/shawarma/data/test_project',
-	'name': 'TEST_PROJECT',
-	'annotations': '/home/shawarma/data/test_project/annotations.csv',
-	'dataset_config': '/home/shawarma/data/test_datasets.json',
-	'datasets': ['TEST2', 'TMA'],
-	'models_dir': '/home/shawarma/data/test_project/models',
-	'tile_um': 302,
-	'tile_px': 299,
-	'use_fp16': True,
-	'batch_train_config': '/home/shawarma/data/test_project/batch_train.csv',
-	'validation_fraction': 0.2,
-	'validation_target': 'per-patient',
-	'validation_strategy': 'k-fold',
-	'validation_k_fold': 3,
-}
+		self.SAVED_MODEL = join(self.PROJECT['models_dir'], 'category1-performance-kfold1', 'trained_model_epoch1.h5')
+		self.REFERENCE_MODEL = None
 
-ANNOTATIONS = [
-	[TCGA.patient, 'dataset', 'category1', 'category2', 'linear1', 'linear2', 'slide'],
-	['234839', 'TEST2', 'PTC-follicular', 'BRAF', '1.1', '1.2', '234839'],
-	['234834', 'TEST2', 'PTC-follicular', 'BRAF', '2.1', '2.2', '234834'],
-	['234809', 'TEST2', 'PTC-follicular', 'BRAF', '2.2', '1.2', ''],
-	['234840', 'TEST2', 'PTC-follicular', 'BRAF', '2.8', '4.2', '234840'],
-	['234832', 'TEST2', 'PTC-follicular', 'Non-mutant', '4.3', '3.2', ''],
-	['234803', 'TEST2', 'PTC-follicular', 'Non-mutant', '2.2', '1.2', ''],
-	['234823', 'TEST2', 'PTC-follicular', 'Non-mutant', '0.2', '1.1', ''],
-	['234833', 'TEST2', 'PTC-follicular', 'Non-mutant', '7.2', '4.2', ''],
+		# Verify slides
+		existing_slides = [sfutil.path_to_name(f) for f in os.listdir(join(root, 'slides')) if sfutil.path_to_ext(f).lower() == 'svs']
 
-	['234798', 'TEST2', 'NIFTP', 'cat2a', '2.8', '4.8', ''],
-	['234808', 'TEST2', 'NIFTP', 'cat2b', '2.8', '4.7', ''],
-	['234810', 'TEST2', 'NIFTP', 'cat2a', '3.8', '4.6', ''],
-	['234829', 'TEST2', 'NIFTP', 'cat2b', '4.8', '4.5', ''],
-	['234843', 'TEST2', 'NIFTP', 'cat2a', '5.8', '4.4', ''],
-	['234851', 'TEST2', 'NIFTP', 'cat2b', '6.8', '4.2', ''],
-	['234867', 'TEST2', 'NIFTP', 'cat2a', '7.8', '4.1', ''],
-
-	['TMA_1185', 'TMA', 'PRAD', 'None', '7.8', '4.1', ''],
-]
-
-SLIDES_TO_VERIFY = ['234834', '234840']
-
-SAVED_MODEL = join(PROJECT_CONFIG['models_dir'], 'category1-performance-kfold1', 'trained_model_epoch1.h5')
-REFERENCE_MODEL = None
-
-# --------------------------------------------------------------------------------------
+		for slide in self.SLIDES:
+			if slide not in existing_slides:
+				self.download_tcga(uuid=self.SLIDES[slide], 
+								   dest=join(root, 'slides'),
+								   message=f"Downloading {sfutil.green(slide)} from TCGA...")
+		log.complete("Slide verification complete.")
+	
+	def download_tcga(self, uuid, dest, message=None):
+		params = {'ids': [uuid]}
+		data_endpt = "https://api.gdc.cancer.gov/data"
+		response = requests.post(data_endpt, data = json.dumps(params), headers = {"Content-Type": "application/json"}, stream=True)
+		response_head_cd = response.headers["Content-Disposition"]
+		block_size = 1024
+		file_size = int(response.headers.get('Content-Length', None))
+		pb = ProgressBar(file_size, leadtext=message)
+		file_name = join(dest, re.findall("filename=(.+)", response_head_cd)[0])
+		with open(file_name, "wb") as output_file:
+			for i, chunk in enumerate(response.iter_content(chunk_size=block_size)):
+				output_file.write(chunk)
+				pb.increase_bar_value(block_size)
+		pb.end()
 
 class TestSuite:
 	'''Class to supervise standardized testing of slideflow pipeline.'''
-	def __init__(self, reset=True, silent=True, buffer=None, num_threads=8, debug=False):
+	def __init__(self, root, reset=True, buffer=None, num_threads=8, debug=False):
 		'''Initialize testing models.'''
 		
 		# Set logging level
 		if debug:
 			os.environ['TF_CPP_MIN_LOG_LEVEL'] = "0"
-	   		tf.get_logger().setLevel("INFO")
+			tf.get_logger().setLevel("INFO")
 		else:
-			os.environ['TF_CPP_MIN_LOG_LEVEL'] = "2"
+			os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
 			tf.get_logger().setLevel("ERROR")
+
+		# Configure testing environment
+		self.config = TestConfigurator(root)
 
 		# Reset test progress
 		if reset: self.reset()
 
 		# Intiailize project
-		self.SFP = sf.SlideflowProject(PROJECT_CONFIG['root'], interactive=False)
+		self.SFP = sf.SlideflowProject(self.config.PROJECT['root'], interactive=False)
 		self.SFP.FLAGS['num_threads'] = num_threads
 		self.configure_project()
 
@@ -136,49 +160,49 @@ class TestSuite:
 
 	def reset(self):
 		log.header("Resetting test project...")
-		if os.path.exists(PROJECT_CONFIG['dataset_config']):
-			os.remove(PROJECT_CONFIG['dataset_config'])
-		if os.path.exists(PROJECT_CONFIG['root']):
-			shutil.rmtree(PROJECT_CONFIG['root'])
-		for dataset_name in TEST_DATASETS.keys():
-			if os.path.exists(TEST_DATASETS[dataset_name]['tiles']):
-				shutil.rmtree(TEST_DATASETS[dataset_name]['tiles'])
-			if os.path.exists(TEST_DATASETS[dataset_name]['tfrecords']):
-				print(f"Removing {TEST_DATASETS[dataset_name]['tfrecords']}")
-				shutil.rmtree(TEST_DATASETS[dataset_name]['tfrecords'])
+		if os.path.exists(self.config.PROJECT['dataset_config']):
+			os.remove(self.config.PROJECT['dataset_config'])
+		if os.path.exists(self.config.PROJECT['root']):
+			shutil.rmtree(self.config.PROJECT['root'])
+		for dataset_name in self.config.DATASETS.keys():
+			if os.path.exists(self.config.DATASETS[dataset_name]['tiles']):
+				shutil.rmtree(self.config.DATASETS[dataset_name]['tiles'])
+			if os.path.exists(self.config.DATASETS[dataset_name]['tfrecords']):
+				print(f"Removing {self.config.DATASETS[dataset_name]['tfrecords']}")
+				shutil.rmtree(self.config.DATASETS[dataset_name]['tfrecords'])
 		print("\t...DONE")
 
 	def configure_project(self):
 		log.header("Setting up initial project configuration...")
-		self.SFP.PROJECT = PROJECT_CONFIG
+		self.SFP.PROJECT = self.config.PROJECT
 		self.SFP.save_project()
 		print("\t...DONE")
 
 	def configure_datasets(self):
 		log.header("Setting up test dataset configuration...")
-		for dataset_name in TEST_DATASETS.keys():
-			self.SFP.add_dataset(dataset_name, slides=TEST_DATASETS[dataset_name]['slides'],
-											   roi=TEST_DATASETS[dataset_name]['roi'],
-											   tiles=TEST_DATASETS[dataset_name]['tiles'],
-											   tfrecords=TEST_DATASETS[dataset_name]['tfrecords'],
-											   path=PROJECT_CONFIG['dataset_config'])
+		for dataset_name in self.config.DATASETS.keys():
+			self.SFP.add_dataset(dataset_name, slides=self.config.DATASETS[dataset_name]['slides'],
+											   roi=self.config.DATASETS[dataset_name]['roi'],
+											   tiles=self.config.DATASETS[dataset_name]['tiles'],
+											   tfrecords=self.config.DATASETS[dataset_name]['tfrecords'],
+											   path=self.SFP.PROJECT['dataset_config'])
 		print("\t...DONE")
 
 	def configure_annotations(self):
 		log.header("Testing annotation configuration and slide name associations...")
-		outfile = PROJECT_CONFIG['annotations']
+		outfile = self.SFP.PROJECT['annotations']
 		with open(outfile, 'w') as csv_outfile:
 			csv_writer = csv.writer(csv_outfile, delimiter=',')
-			for an in ANNOTATIONS:
+			for an in self.config.ANNOTATIONS:
 				csv_writer.writerow(an)
 		project_dataset = Dataset(tile_px=299, tile_um=302,
-								  config_file=PROJECT_CONFIG['dataset_config'],
-								  sources=PROJECT_CONFIG['datasets'],
-								  annotations=PROJECT_CONFIG['annotations'])
-		project_dataset.update_annotations_with_slidenames(PROJECT_CONFIG['annotations'])
+								  config_file=self.SFP.PROJECT['dataset_config'],
+								  sources=self.SFP.PROJECT['datasets'],
+								  annotations=self.SFP.PROJECT['annotations'])
+		project_dataset.update_annotations_with_slidenames(self.SFP.PROJECT['annotations'])
 		loaded_slides = project_dataset.get_slides()
-		for slide in SLIDES_TO_VERIFY:
-			if slide not in loaded_slides:
+		for slide in [row[0] for row in self.config.ANNOTATIONS[1:]]:
+			if slide not in [sfutil._shortname(l) for l in loaded_slides]:
 				log.error(f"Failed to correctly associate slide names ({slide}); please see annotations file below.")
 				with open(outfile, 'r') as ann_read:
 					print()
@@ -191,7 +215,7 @@ class TestSuite:
 		# Remove old batch train file
 		log.header("Setting up hyperparameter setup...")
 		try:
-			os.remove(PROJECT_CONFIG['batch_train_config'])
+			os.remove(self.SFP.PROJECT['batch_train_config'])
 		except:
 			pass
 		# Setup loss function
@@ -220,7 +244,7 @@ class TestSuite:
 											 balanced_validation=["NO_BALANCE"],
 											 augment=[True],
 											 label='TEST',
-											 filename=PROJECT_CONFIG["batch_train_config"])
+											 filename=self.SFP.PROJECT["batch_train_config"])
 
 		# Create single hyperparameter combination
 		hp = sf.model.HyperParameters(finetune_epochs=1, toplayer_epochs=0, model='InceptionV3', pooling='max', loss=loss,
@@ -234,7 +258,7 @@ class TestSuite:
 		# Test tile extraction, default parameters, for regular slides
 		if regular:
 			log.header("Testing multiple slides extraction...")
-			self.SFP.extract_tiles(tile_px=299, tile_um=302, buffer=self.buffer, dataset=['TEST2'])
+			self.SFP.extract_tiles(tile_px=299, tile_um=302, buffer=self.buffer, dataset=['TEST'])
 		if tma:
 			log.header("Testing Tumor Micro-array (TMA) extraction...")
 			self.SFP.extract_tiles(tile_px=299, tile_um=302, buffer=self.buffer, dataset=['TMA'], tma=True)
@@ -256,7 +280,7 @@ class TestSuite:
 	def test_realtime_normalizer(self):
 		print("Testing realtime normalization, using Macenko")
 		hp = self.setup_hp('categorical')
-		self.SFP.train(outcome_header='category1', k_fold_iter=1, normalizer='macenko', normalizer_strategy='realtime', steps_per_epoch_override=5)
+		self.SFP.train(outcome_header='category1', k_fold_iter=1, normalizer='reinhard', normalizer_strategy='realtime', steps_per_epoch_override=5)
 
 	def test_training(self, categorical=True, linear=True, multi_input=True):
 		if categorical:
@@ -295,7 +319,7 @@ class TestSuite:
 
 	def test_evaluation(self):
 		log.header("Testing evaluation of a saved model...")
-		self.SFP.evaluate(outcome_header='category1', model=SAVED_MODEL)
+		self.SFP.evaluate(outcome_header='category1', model=self.config.SAVED_MODEL)
 		#log.header("Testing that evaluation matches known baseline...")
 		#self.SFP.evaluate(outcome_header='category1', model=REFERENCE_MODEL, filters={'submitter_id': '234839'})
 		# Code to lookup excel sheet of predictions and verify they match known baseline
@@ -303,25 +327,25 @@ class TestSuite:
 
 	def test_heatmap(self):
 		log.header("Testing heatmap generation...")
-		self.SFP.generate_heatmaps(SAVED_MODEL, filters={TCGA.patient: ['234839']})
+		self.SFP.generate_heatmaps(self.config.SAVED_MODEL, filters={sfutil.TCGA.patient: ['234839']})
 		print("\t...OK")
 
 	def test_mosaic(self):
 		log.header("Testing mosaic generation...")
-		self.SFP.generate_mosaic(SAVED_MODEL, mosaic_filename="mosaic_test.png")
+		self.SFP.generate_mosaic(self.config.SAVED_MODEL, mosaic_filename="mosaic_test.png")
 		print("\t...OK")
 
 	def test_activations(self):
 		log.header("Testing activations analytics...")
-		AV = self.SFP.generate_activations_analytics(model=SAVED_MODEL, 
+		AV = self.SFP.generate_activations_analytics(model=self.config.SAVED_MODEL, 
 													outcome_header='category1', 
 													focus_nodes=[0])
 		AV.generate_box_plots()
 		umap = TFRecordMap.from_activations(AV)
-		umap.save_2d_plot(join(PROJECT_CONFIG['root'], 'stats', '2d_umap.png'))
+		umap.save_2d_plot(join(self.SFP.PROJECT['root'], 'stats', '2d_umap.png'))
 		top_nodes = AV.get_top_nodes_by_slide()
 		for node in top_nodes[:5]:
-			umap.save_3d_plot(node=node, filename=join(PROJECT_CONFIG['root'], 'stats', f'3d_node{node}.png'))
+			umap.save_3d_plot(node=node, filename=join(self.SFP.PROJECT['root'], 'stats', f'3d_node{node}.png'))
 		print("\t...OK")
 
 	def test(self, extract=True, train=True, normalizer=True, train_performance=True, 
