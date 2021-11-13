cfda049d9a14d80d43458bf65a26617ac55ab043
James <jamesmdolezal@gmail.com>
2021-11-12 23:39:56 -0600

    Github workflow & setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     package_data = {
