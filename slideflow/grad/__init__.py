9a379a2d95a2117bbf0c2f7dfb7709b92dfda407
James Dolezal <jamesmdolezal@gmail.com>
2022-06-20 16:26:55 -0600

    New multi_plot with adjacent and overlay display

--- slideflow/grad/__init__.py
+++ slideflow/grad/__init__.py
@@ -8,8 +8,9 @@ from slideflow.util import batch
 import saliency.core as saliency
 
 class SaliencyMap:
-    def __init__(self, model):
+    def __init__(self, model, class_idx):
         self.model = model
+        self.class_idx = class_idx
         self.gradients = saliency.GradientSaliency()
         self.ig = saliency.IntegratedGradients()
         self.guided_ig = saliency.GuidedIG()
@@ -25,7 +26,7 @@ class SaliencyMap:
             if expected_keys == [saliency.base.INPUT_OUTPUT_GRADIENTS]:
                 # For vanilla gradient, Integrated Gradients, XRAI
                 tape.watch(image)
-                output = self.model(image)[:, 0]
+                output = self.model(image)[:, self.class_idx]
                 gradients = tape.gradient(output, image)
                 return {saliency.base.INPUT_OUTPUT_GRADIENTS: gradients}
             else:
@@ -125,21 +126,55 @@ def comparison_plot(original, maps, cmap=plt.cm.gray):
         ax[ax_idx[i+1][0], ax_idx[i+1][1]].axis('off')
         ax[ax_idx[i+1][0], ax_idx[i+1][1]].imshow(map_img, cmap=cmap, vmin=0, vmax=1)
         ax[ax_idx[i+1][0], ax_idx[i+1][1]].set_title(map_name)
-
-def side_by_side_plot(originals, maps, n_rows=4, cmap=plt.cm.gray):
+        
+def multi_plot(originals, maps, method='adjacent', n_rows=4, cmap='inferno'):
+    assert method in ('adjacent', 'overlay')
     orig_batch = list(batch(originals, n_rows))
     maps_batch = list(batch(maps, n_rows))
-    n_cols = len(orig_batch)*2
+    n_cols = len(orig_batch) * 4
     scale = 5
-    fig, ax = plt.subplots(n_rows, n_cols, figsize=(n_cols * scale, n_rows * scale))
-
+    
+    gridspec = dict(hspace=0.0, width_ratios=[1, 1, 1, 0.2, 1, 1, 1, 0.2])
+    figsize = (n_cols * scale * 0.79, n_rows * scale)
+    fig, ax = plt.subplots(n_rows, n_cols, figsize=figsize, gridspec_kw=gridspec)
+    
+    def remove_ticks(axis):
+        axis.spines.right.set_visible(False)
+        axis.spines.top.set_visible(False)
+        axis.spines.left.set_visible(False)
+        axis.spines.bottom.set_visible(False)
+        axis.set_xticklabels([])
+        axis.set_xticks([])
+        axis.set_yticklabels([])
+        axis.set_yticks([])
+        
     for c in range(len(orig_batch)):
         for r in range(n_rows):
-            ax[r, c*2].axis('off') 
-            ax[r, c*2].imshow(orig_batch[c][r])
-            ax[r, c*2].set_aspect('equal')
-            ax[r, (c*2)+1].axis('off')
-            ax[r, (c*2)+1].imshow(maps_batch[c][r], cmap=cmap, vmin=0, vmax=1)
-            ax[r, (c*2)+1].set_aspect('equal')
+            _c = c * 4
+            
+            ax[r, _c+3].set_visible(False)
+            
+            remove_ticks(ax[r, _c])
+            ax[r, _c].set_ylabel(f"{(c*3)+r}")
+            ax[r, _c].imshow(orig_batch[c][r])
+            ax[r, _c].set_aspect('equal')
+            ax
+            
+            ax[r, _c+1].axis('off') 
+            ax[r, _c+1].imshow(orig_batch[c][r])
+            ax[r, _c+1].set_aspect('equal')
+            
+            ax[r, _c+1].axis('off')
+            ax[r, _c+1].imshow(maps_batch[c][r], cmap='Oranges', vmin=0, vmax=1, alpha=0.6)
+            ax[r, _c+1].set_aspect('equal')
+            
+            ax[r, _c+2].axis('off')
+            ax[r, _c+2].imshow(maps_batch[c][r], cmap=cmap, vmin=0, vmax=1)
+            ax[r, _c+2].set_aspect('equal')
+            
+            if r == 0:
+                ax[r, _c].set_title("Original")
+                ax[r, _c+1].set_title("Overlay")
+                ax[r, _c+2].set_title("Saliency Map")
         
     fig.subplots_adjust(wspace=0, hspace=0)
