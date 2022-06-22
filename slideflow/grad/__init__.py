10478e76a7933d46a8f28e81dacf5ea6d5ba2845
James <jamesmdolezal@gmail.com>
2022-06-22 16:37:35 -0500

    Formatting; bug fix

--- slideflow/grad/__init__.py
+++ slideflow/grad/__init__.py
@@ -1,11 +1,12 @@
-import tensorflow as tf
-import numpy as np
-from matplotlib import pyplot as plt
 #from matplotlib.widgets import MultiCursor
 from itertools import chain
+
+import numpy as np
+import saliency.core as saliency
+import tensorflow as tf
+from matplotlib import pyplot as plt
 from slideflow.util import batch
 
-import saliency.core as saliency
 
 class SaliencyMap:
     def __init__(self, model, class_idx):
@@ -32,7 +33,7 @@ class SaliencyMap:
             else:
                 # For Grad-CAM
                 raise ValueError
-    
+
     def _calc_vanilla_map(self, img, mask_fn, **kwargs):
         mask_3d = mask_fn(img, self.grad_fn, **kwargs)
         return saliency.VisualizeImageGrayscale(mask_3d)
@@ -61,11 +62,11 @@ class SaliencyMap:
     def _calc_blur_ig_map(self, img, mask_fn, batch_size=20):
         mask_3d = mask_fn(img, self.grad_fn, batch_size=batch_size)
         return saliency.VisualizeImageGrayscale(mask_3d)
-    
+
     def all_maps(self, img):
         return {
             'Vanilla': self.vanilla_map(img),
-            'Vanilla (Smoothed)': self.vanilla_map_smooth(img), 
+            'Vanilla (Smoothed)': self.vanilla_map_smooth(img),
             'Integrated Gradients': self.integrated_gradients_map(img),
             'Integrated Gradients (Smooth)': self.integrated_gradients_map_smooth(img),
             'Guided Integrated Gradients': self.guided_integrated_gradients_map(img),
@@ -79,19 +80,19 @@ class SaliencyMap:
 
     def vanilla_map_smooth(self, img, **kwargs):
         return self._calc_vanilla_map(img, self.gradients.GetSmoothedMask, **kwargs)
-    
+
     def integrated_gradients_map(self, img, **kwargs):
         return self._calc_ig_map(img, self.ig.GetMask, **kwargs)
 
     def integrated_gradients_map_smooth(self, img, **kwargs):
         return self._calc_ig_map(img, self.ig.GetSmoothedMask, **kwargs)
-        
+
     def guided_integrated_gradients_map(self, img, **kwargs):
         return self._calc_guided_ig_map(img, self.guided_ig.GetMask, **kwargs)
 
     def guided_integrated_gradients_map_smooth(self, img, **kwargs):
         return self._calc_guided_ig_map(img, self.guided_ig.GetSmoothedMask, **kwargs)
-        
+
     def blur_integrated_gradients_map(self, img, **kwargs):
         return self._calc_blur_ig_map(img, self.blur_ig.GetMask, **kwargs)
 
@@ -100,13 +101,13 @@ class SaliencyMap:
 
     def xrai_map(self, img, batch_size=20, **kwargs):
         return self.xrai.GetMask(img, self.grad_fn, batch_size=batch_size, **kwargs)
-    
+
     def xrai_map_fast(self, img, batch_size=20, **kwargs):
         return self.xrai.GetMask(
-            img, 
-            self.grad_fn, 
-            batch_size=batch_size, 
-            extra_parameters=self.fast_xrai_params, 
+            img,
+            self.grad_fn,
+            batch_size=batch_size,
+            extra_parameters=self.fast_xrai_params,
             **kwargs)
 
 
@@ -117,27 +118,29 @@ def comparison_plot(original, maps, cmap=plt.cm.gray):
     ax_idx = [[i, j] for i in range(n_rows) for j in range(n_cols)]
     fig, ax = plt.subplots(n_rows, n_cols, figsize=(n_rows * scale, n_cols * scale))
     #multi = MultiCursor(fig.canvas, list(chain.from_iterable(ax)), color='r', lw=1)
-    
-    ax[ax_idx[0][0], ax_idx[0][1]].axis('off') 
+
+    ax[ax_idx[0][0], ax_idx[0][1]].axis('off')
     ax[ax_idx[0][0], ax_idx[0][1]].imshow(original)
     ax[ax_idx[0][0], ax_idx[0][1]].set_title('Original')
-    
+
     for i, (map_name, map_img) in enumerate(maps.items()):
         ax[ax_idx[i+1][0], ax_idx[i+1][1]].axis('off')
         ax[ax_idx[i+1][0], ax_idx[i+1][1]].imshow(map_img, cmap=cmap, vmin=0, vmax=1)
         ax[ax_idx[i+1][0], ax_idx[i+1][1]].set_title(map_name)
-        
+
+    fig.subplots_adjust(wspace=0, hspace=0)
+
 def multi_plot(originals, maps, method='adjacent', n_rows=4, cmap='inferno'):
     assert method in ('adjacent', 'overlay')
     orig_batch = list(batch(originals, n_rows))
     maps_batch = list(batch(maps, n_rows))
     n_cols = len(orig_batch) * 4
     scale = 5
-    
-    gridspec = dict(hspace=0.0, width_ratios=[1, 1, 1, 0.2, 1, 1, 1, 0.2])
+
+    gridspec = dict(hspace=0.0, width_ratios=[1, 1, 1, 0.2]*len(orig_batch))
     figsize = (n_cols * scale * 0.79, n_rows * scale)
     fig, ax = plt.subplots(n_rows, n_cols, figsize=figsize, gridspec_kw=gridspec)
-    
+
     def remove_ticks(axis):
         axis.spines.right.set_visible(False)
         axis.spines.top.set_visible(False)
@@ -147,34 +150,34 @@ def multi_plot(originals, maps, method='adjacent', n_rows=4, cmap='inferno'):
         axis.set_xticks([])
         axis.set_yticklabels([])
         axis.set_yticks([])
-        
+
     for c in range(len(orig_batch)):
-        for r in range(n_rows):
+        for r in range(len(orig_batch[c])):
             _c = c * 4
-            
+
             ax[r, _c+3].set_visible(False)
-            
+
             remove_ticks(ax[r, _c])
-            ax[r, _c].set_ylabel(f"{(c*3)+r}")
+            ax[r, _c].set_ylabel(f"{(c*4)+r}")
             ax[r, _c].imshow(orig_batch[c][r])
             ax[r, _c].set_aspect('equal')
             ax
-            
-            ax[r, _c+1].axis('off') 
+
+            ax[r, _c+1].axis('off')
             ax[r, _c+1].imshow(orig_batch[c][r])
             ax[r, _c+1].set_aspect('equal')
-            
+
             ax[r, _c+1].axis('off')
             ax[r, _c+1].imshow(maps_batch[c][r], cmap='Oranges', vmin=0, vmax=1, alpha=0.6)
             ax[r, _c+1].set_aspect('equal')
-            
+
             ax[r, _c+2].axis('off')
             ax[r, _c+2].imshow(maps_batch[c][r], cmap=cmap, vmin=0, vmax=1)
             ax[r, _c+2].set_aspect('equal')
-            
+
             if r == 0:
                 ax[r, _c].set_title("Original")
                 ax[r, _c+1].set_title("Overlay")
                 ax[r, _c+2].set_title("Saliency Map")
-        
+
     fig.subplots_adjust(wspace=0, hspace=0)
