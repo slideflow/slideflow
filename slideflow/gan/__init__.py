68ae6de3740cb4c6f278b14123bc3b575e4ad1fb
James <jamesmdolezal@gmail.com>
2022-06-26 07:12:04 -0500

    Update stylegan submodule commit

---
+++ slideflow/gan/__init__.py
@@ -0,0 +1,4 @@
+import slideflow.gan.gan_utils
+import slideflow.gan.stylegan2
+from slideflow.gan.stylegan2 import embedding, plot
+from slideflow.gan.stylegan2.utils import decode_batch, process_gan_batch
