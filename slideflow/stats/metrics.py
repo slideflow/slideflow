79972594dcb206203058fdefc48e9670813592a3
prajvalmohan23 <97634166+prajvalmohan23@users.noreply.github.com>
2022-08-11 10:56:08 -0500

    Edge case handling for fit.auroc, fit.ap, fit.opt_thresh == nan (#181)
    Handle edge cases where fit.auroc, fit.ap and fit.opt_thresh can be none or np.nan

--- slideflow/stats/metrics.py
+++ slideflow/stats/metrics.py
@@ -257,11 +257,17 @@ def categorical_metrics(
                 fit.save_prc(data_dir, f"{label_start}{outcome}_tile_PRC{i}")
                 all_auc[outcome] += [fit.auroc]
                 all_ap[outcome] += [fit.ap]
-                log.info(
-                    f"{level}-level AUC (cat #{i:>2}): {fit.auroc:.3f} "
-                    f"{level}-level AP: {fit.ap:.3f} (opt. threshold: "
-                    f"{fit.opt_thresh:.3f})"
+                if any(val in (np.nan, None) for val in (fit.auroc, fit.ap, fit.opt_thresh)):
+                    log.info(
+                    f"{level}-level AUC (cat #{i:>2}): None "
+                    f"{level}-level AP: None"
                 )
+                else:
+                    log.info(
+                        f"{level}-level AUC (cat #{i:>2}): {fit.auroc:.3f} "
+                        f"{level}-level AP: {fit.ap:.3f} (opt. threshold: "
+                        f"{fit.opt_thresh:.3f})"
+                    )
         except ValueError as e:
             # Occurs when predictions contain NaN
             log.error(f'Error encountered when generating AUC: {e}')
