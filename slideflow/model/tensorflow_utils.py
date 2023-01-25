slideflow/slideflow

commit 125c2fc84e02ad264b3726d4c096b943e0e7021e

Core fixes
- Add NaN handling during Tensorflow loss calculation
- Sort slide paths displayed when Project is loaded in Workbench

slideflow/model/tensorflow_utils.py
@@ -409,13 +409,15 @@ def eval_from_model(
                 yt = [yt[f'out-{o}'] for o in range(len(yt))]
                 if loss is not None:
                     loss_val = [loss(yt[i], yp[i]) for i in range(len(yt))]
+                    loss_val = [l for l in loss_val if not tf.math.is_nan(l)]
                     batch_loss = tf.math.reduce_sum(loss_val).numpy()
                     running_loss = (((num_vals - slide.shape[0]) * running_loss) + batch_loss) / num_vals
             else:
                 y_true += [yt.numpy()]
                 if loss is not None:
                     loss_val = loss(yt, yp)
-                    batch_loss = tf.math.reduce_sum(loss_val).numpy()
+                    not_nan = ~tf.math.is_nan(loss_val)
+                    batch_loss = tf.math.reduce_sum(loss_val[not_nan]).numpy()
                     running_loss = (((num_vals - slide.shape[0]) * running_loss) + batch_loss) / num_vals
 
     if verbosity != 'silent':

slideflow/workbench/project_widget.py
@@ -67,7 +67,7 @@ class ProjectWidget:
 
             sf.log.debug("Loading project at {}...".format(project))
             self.P = sf.Project(project)
-            self.slide_paths = self.P.dataset().slide_paths()
+            self.slide_paths = sorted(self.P.dataset().slide_paths())
             viz.model_widget.search_dirs = [self.P.models_dir]
             viz.slide_widget.project_slides = self.slide_paths
             self.viz.create_toast(f"Loaded project at {project}", icon="success")

commit 3f674f5ff2ff9fdf5519620dc33740f1d249d4d5

Fix bug with evaluating multi-class Tensorflow models
- Fix `truth value of an array is ambiguous` error when evaluating multi-class Tensorflow models, recently introduced in 125c2fc

slideflow/model/tensorflow_utils.py
@@ -409,15 +409,20 @@ def eval_from_model(
                 yt = [yt[f'out-{o}'] for o in range(len(yt))]
                 if loss is not None:
                     loss_val = [loss(yt[i], yp[i]) for i in range(len(yt))]
-                    loss_val = [l for l in loss_val if not tf.math.is_nan(l)]
+                    loss_val = [tf.boolean_mask(l, tf.math.is_finite(l)) for l in loss_val]
                     batch_loss = tf.math.reduce_sum(loss_val).numpy()
                     running_loss = (((num_vals - slide.shape[0]) * running_loss) + batch_loss) / num_vals
             else:
                 y_true += [yt.numpy()]
                 if loss is not None:
                     loss_val = loss(yt, yp)
-                    not_nan = ~tf.math.is_nan(loss_val)
-                    batch_loss = tf.math.reduce_sum(loss_val[not_nan]).numpy()
+                    if tf.rank(loss_val):
+                        # Loss is a vector
+                        is_finite = tf.math.is_finite(loss_val)
+                        batch_loss = tf.math.reduce_sum(tf.boolean_mask(loss_val, is_finite)).numpy()
+                    else:
+                        # Loss is a scalar
+                        batch_loss = loss_val.numpy()  # type: ignore
                     running_loss = (((num_vals - slide.shape[0]) * running_loss) + batch_loss) / num_vals
 
     if verbosity != 'silent':