Fix bug when using StyleGAN2Interpolator with Macenko normalization

slideflow/gan/interpolate.py
@@ -170,7 +170,7 @@ class StyleGAN2Interpolator:
         if sf.backend() == 'tensorflow':
             import tensorflow as tf
 
-            sig = tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.uint8)
+            sig = tf.TensorSpec(shape=(None, self.target_px, self.target_px, 3), dtype=tf.uint8)
             dts = tf.data.Dataset.from_generator(generator, output_signature=sig)
             return dts.map(
                 partial(

slideflow/norm/tensorflow/macenko.py
@@ -287,8 +287,11 @@ class MacenkoNormalizer:
         Returns:
             tf.Tensor: Normalized image.
         """
-        if len(I.shape) != 3:
+        if len(I.shape) == 4:
+            return tf.map_fn(self.transform, I)
+        elif len(I.shape) == 3:
+            return transform(I, self.stain_matrix_target, self.target_concentrations)
+        else:
             raise ValueError(
-                f"Invalid shape for transform(): expected 3, got {I.shape}"
+                f"Invalid shape for transform(): expected 3 or 4, got {I.shape}"
             )
-        return transform(I, self.stain_matrix_target, self.target_concentrations)