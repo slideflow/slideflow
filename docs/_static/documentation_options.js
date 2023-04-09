a0f8b94e90e82f8b91380f53a3a333e304902bfd
James <jamesmdolezal@gmail.com>
2023-04-09 09:43:04 -0500

    Update version reported in Docs title

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.0-beta2',
+    VERSION: '2.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
