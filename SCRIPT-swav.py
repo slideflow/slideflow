84b5bcefef13b6493ff30f435e2491036182e3d7
matte-esse <matt.s974@gmail.com>
2023-04-18 09:28:31 -0500

    add script

---
+++ SCRIPT-swav.py
@@ -0,0 +1,22 @@
+import slideflow as sf
+from slideflow import swav
+
+project_path = '/home/mattsacco/data/PROJECTS/UPenn_debug'
+
+P = sf.Project(root=project_path)
+dataset = P.dataset(tile_px=299, tile_um=604)
+
+swav_args = swav.get_args(
+  warmup_epochs=1,
+  epochs=2,
+  nmb_crops=[2, 6],
+  size_crops=[299, 96],
+  min_scale_crops=[0.14, 0.05],
+  max_scale_crops=[1., 0.14],
+  batch_size=32
+)
+
+P.train_swav(
+  swav_args=swav_args,
+  train_dataset=dataset,
+)
