de7d3ff9cf87d07c82bf4da0a3d3ddef4730743e
James <james.dolezal@uchospitals.edu>
2023-04-23 11:44:35 -0500

    Auto-use MPS on MacOS if available.

--- slideflow/model/torch_utils.py
+++ slideflow/model/torch_utils.py
@@ -445,6 +445,10 @@ def predict_from_model(
 def get_device(device: Optional[str] = None):
     if device is None and torch.cuda.is_available():
         return torch.device('cuda')
+    elif (device is None 
+          and hasattr(torch.backends, 'mps') 
+          and torch.backends.mps.is_available()):
+        return torch.device('mps')
     elif device is None:
         return torch.device('cpu')
     elif isinstance(device, str):
