1360bb5e172b54662c1d7046e78584cba26c1d3e
Emma Dyer <ecdyer@sbcglobal.net>
2023-05-25 15:14:38 -0600

    Add _calculate_meta_viz()

--- slideflow/stats/slidemap.py
+++ slideflow/stats/slidemap.py
@@ -9,6 +9,10 @@ import warnings
 from os.path import join, exists, isdir
 from pandas.core.frame import DataFrame
 from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances
+from sklearn.neighbors import NearestNeighbors
+from sklearn.manifold import TSNE, MDS
+from scipy.linalg import eigh
 from slideflow import errors
 from slideflow.stats import stats_utils
 from slideflow.util import log
@@ -143,6 +147,13 @@ class SlideMap:
     Slides are mapped in 2D either explicitly with pre-specified coordinates,
     or with dimensionality reduction from post-convolutional layer weights,
     provided from :class:`slideflow.DatasetFeatures`.
+    
+    Attributes
+    ----------
+    _meta_viz : This method was written by Kevin Sun and is based
+    on the following paper: Ma, R., Sun, E., and Zou, J. (2022) A Spectral Method for 
+    Assessing and Combining Multiple Data Visualizations.
+    
     """
 
     def __init__(
@@ -577,6 +588,89 @@ class SlideMap:
             })
         self.data = pd.DataFrame(data_dict)
 
+    def _calculate_meta_viz(self, 
+                            visualizations: List[np.ndarray], 
+                            projection_method: Union[str, None] = None
+                            ) -> Tuple[np.ndarray, np.ndarray]:
+        """ Internal function to calculate the meta vizualization.
+        
+        Args: 
+            vizualizations (list(np.ndarray)): list of numpy arrays of size n x 2 
+            (columns being the two-dimensional coordinates of visualization). Each row 
+            should be matched to the same observation/sample across all elements in the list
+            projection_method (Union[str, None]): options are "tSNE" or "MDS" for 
+            reprojecting the meta-distance matrix into a meta-visualization
+        
+        Returns:
+            meta_visualization (np.ndarray): size nx2 two-dimensional visualization 
+            genereated using the meta-visualization approach. Returned only if 
+            projection_method is not None
+            meta_distances (np.ndarray): size nxn pairwise euclidean distance matrix
+            generated using meta-visualization approach
+        
+        Approach:
+            1. Computes euclidean distance matrices from 2D embeddings
+            2. Ensembles the euclidean distance according to eigenvector with largest eigenvalue
+            3. Uses projection_method ["MDS", "tSNE"] to transform euclidean 
+            distance matrix to 2D embeddings
+        
+        This method was written by Kevin Sun and is based
+        on the following paper: Ma, R., Sun, E., and Zou, J. (2022) A Spectral 
+        Method for Assessing and Combining Multiple Data Visualizations.     
+        """
+            # define number of samples
+        n = visualizations[0].shape[0]
+        K = len(visualizations)
+        
+        # Iterate and record distance matrices
+        X_distance_matrix_list = []
+        
+        # compute pairwise distance matrix
+        for X_embedded in visualizations:
+            assert X_embedded.shape[0] == n, "All visualization arrays need to have the same number of rows"
+            assert X_embedded.shape[1] == 2, "All visualization arrays need to have two columns"
+            X_distance = pairwise_distances(X_embedded)
+            X_distance_matrix_list.append(X_distance)
+                
+        # Compute weights for meta-visualization
+        meta_distances = np.zeros((n,n))
+        weights = np.zeros((n,K))
+        for j in range(n):
+            # fill in comparison matrix
+            comp_mat = np.zeros((K,K))
+            for i in range(K):
+                for k in range(K):
+                    comp_mat[i,k] = np.sum(X_distance_matrix_list[k][:,j]*X_distance_matrix_list[i][:,j])/np.sqrt(np.sum(X_distance_matrix_list[k][:,j]**2))/np.sqrt(np.sum(X_distance_matrix_list[i][:,j]**2))
+            # Eigenscore
+            w, v = np.linalg.eig(comp_mat)
+            weights[j,:] = np.abs(v[:,0])
+        
+            # Ensembles distance matrices
+            matrix_norms = []
+            for i in range(K):
+                matrix_norms.append(X_distance_matrix_list[i][:,j]/np.sqrt(np.sum(X_distance_matrix_list[i][:,j]**2)))
+            
+            temp = np.zeros(matrix_norms[0].shape)
+            for i in range(K):
+                temp += matrix_norms[i]*weights[j,i]
+                
+            meta_distances[:,j] = temp
+        
+        meta_distances = np.nan_to_num((meta_distances+meta_distances.T)/2)
+        
+        # Re-project on 2D embedding space
+        if projection_method is None:
+            return(meta_distances)
+        else:
+            if projection_method == "tSNE":
+                tsne = TSNE(n_components=2, metric="precomputed", verbose=0).fit(meta_distances)
+                meta_visualization = tsne.embedding_
+            elif projection_method == "MDS":
+                mds = MDS(n_components=2, verbose=0, dissimilarity="precomputed").fit(meta_distances)
+                meta_visualization = mds.embedding_
+        
+            return(meta_visualization, meta_distances)
+
     def activations(self) -> np.ndarray:
         """Return associated DatasetFeatures activations as a numpy array
         corresponding to the points on this SlideMap."""
@@ -808,7 +902,7 @@ class SlideMap:
 
         return normalized
 
-    def meta_visualization(
+    def meta_viz(
         self, 
         dim_red_methods: Optional[Dict[str, Dict[str, Union[int, str, bool]]]]):
         """ Creates a spectral meta-visualization by combining
