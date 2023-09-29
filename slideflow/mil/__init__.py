537ce9bb7cb3b89aa07ab0ffbea09ac6c169599f
James <jamesmdolezal@gmail.com>
2023-09-29 16:38:48 -0500

    Add multimodal functions to `sf.mil`

--- slideflow/mil/__init__.py
+++ slideflow/mil/__init__.py
@@ -1,5 +1,6 @@
-from .train import train_mil, train_clam, train_fastai, build_fastai_learner
-from .eval import eval_mil, predict_slide
+from .train import (train_mil, train_clam, train_fastai, build_fastai_learner,
+                    train_multimodal_mil)
+from .eval import eval_mil, predict_slide, eval_multimodal_mil
 from .train._legacy import legacy_train_clam
 from ._params import (
     mil_config, _TrainerConfig, TrainerConfigFastAI, TrainerConfigCLAM,
