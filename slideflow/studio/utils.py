3707a6c22f93e46da725443a3cdafc8ed51a6d59
James Dolezal <jamesmdolezal@gmail.com>
2023-10-05 15:14:14 -0500

    Bug fix for loading pytorch devices in Studio

--- slideflow/studio/utils.py
+++ slideflow/studio/utils.py
@@ -90,7 +90,8 @@ def _load_model_and_saliency(model_path, device=None):
     if sf.util.torch_available and sf.util.path_to_ext(model_path) == 'zip':
         import slideflow.model.torch
         _device = sf.model.torch.torch_utils.get_device()
-        _model = sf.model.torch.load(model_path, map_location=_device)
+        _model = sf.model.torch.load(model_path)
+        _model.to(_device)
         _model.eval()
         if device is not None:
             _model = _model.to(device)
