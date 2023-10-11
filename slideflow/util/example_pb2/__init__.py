8133fbb0e9d18abb4b385eacba919840e4836e66
James Dolezal <jamesmdolezal@gmail.com>
2023-10-11 07:22:20 -0600

    Fix protobuf bug

--- slideflow/util/example_pb2/__init__.py
+++ slideflow/util/example_pb2/__init__.py
@@ -2,7 +2,7 @@ from packaging import version
 from google.protobuf import __version__ as protobuf_version
 
 if version.parse(protobuf_version) < version.parse("3.21"):
-    from ._proto3 import *
+    from ._proto3_pb2 import *
 else:
-    from ._proto4 import *
+    from ._proto4_pb2 import *
 
