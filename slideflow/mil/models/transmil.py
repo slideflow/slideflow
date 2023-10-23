b962b1aa844d561f8c3defb4b554c8ba95c2c02f
James <jamesmdolezal@gmail.com>
2023-10-23 10:20:58 -0500

    Refactor get_last_layer_activations() for TransMIL

--- slideflow/mil/models/transmil.py
+++ slideflow/mil/models/transmil.py
@@ -48,32 +48,7 @@ class TransMIL(nn.Module):
         self.to(get_device())
 
     def forward(self, h):
-        h = self._fc1(h) #[B, n, 1024] -> [B, n, 512]
-
-        #---->pad
-        H = h.shape[1]
-        _H, _W = int(np.ceil(np.sqrt(H))), int(np.ceil(np.sqrt(H)))
-        add_length = _H * _W - H
-        h = torch.cat([h, h[:,:add_length,:]], dim = 1) #[B, N, 512]
-
-        #---->cls_token
-        B = h.shape[0]
-        cls_tokens = self.cls_token.expand(B, -1, -1)
-        h = torch.cat((cls_tokens, h), dim=1)
-
-        #---->Translayer x1
-        h = self.layer1(h) #[B, N, 512]
-
-        #---->PPEG
-        h = self.pos_layer(h, _H, _W) #[B, N, 512]
-
-        #---->Translayer x2
-        h = self.layer2(h) #[B, N, 512]
-
-        #---->cls_token
-        h = self.norm(h)[:,0]
-
-        #---->predict
+        h = self.get_last_layer_activations(h) # [B, n, 1024] -> [B, 512]
         logits = self._fc2(h) #[B, n_classes]
         return logits
 
