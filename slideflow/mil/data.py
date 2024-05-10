088f26070704192cff0c2a4224b75978799c8640
Richard Vu <richardvu@carrot.uchicago.edu>
2024-05-09 22:29:33 -0500

    Change function to draw equal number of random features from different slides

--- slideflow/mil/data.py
+++ slideflow/mil/data.py
@@ -11,7 +11,7 @@ from torch.utils.data import Dataset
 
 # -----------------------------------------------------------------------------
 
-def build_dataset(bags, targets, encoder, bag_size, use_lens=False):
+def build_dataset(bags, targets, encoder, bag_size, use_lens=False, balanced=False):
     assert len(bags) == len(targets)
 
     def _zip(bag, targets):
@@ -23,13 +23,13 @@ def build_dataset(bags, targets, encoder, bag_size, use_lens=False):
 
     dataset = MapDataset(
         _zip,
-        BagDataset(bags, bag_size=bag_size),
+        BagDataset(bags, bag_size=bag_size, balanced=balanced),
         EncodedDataset(encoder, targets),
     )
     dataset.encoder = encoder
     return dataset
 
-def build_clam_dataset(bags, targets, encoder, bag_size):
+def build_clam_dataset(bags, targets, encoder, bag_size, balanced=False):
     assert len(bags) == len(targets)
 
     def _zip(bag, targets):
@@ -38,7 +38,7 @@ def build_clam_dataset(bags, targets, encoder, bag_size):
 
     dataset = MapDataset(
         _zip,
-        BagDataset(bags, bag_size=bag_size),
+        BagDataset(bags, bag_size=bag_size, balanced=balanced),
         EncodedDataset(encoder, targets),
     )
     dataset.encoder = encoder
@@ -65,10 +65,72 @@ def build_multibag_dataset(bags, targets, encoder, bag_size, n_bags, use_lens=Fa
 
 def _to_fixed_size_bag(
     bag: torch.Tensor,
-    bag_size: int = 512
+    bag_size: int = 512,
+    address: dict = dict(),
+    balanced: bool = False
 ) -> Tuple[torch.Tensor, int]:
-    # get up to bag_size elements
-    bag_idxs = torch.randperm(bag.shape[0])[:bag_size]
+    '''
+    Get up to bag_size elements
+    Args:
+        bag (torch.Tensor): feature
+        bag_size (int): the maximize size of the returned features tensor. Defaults to 512
+        address (dict): if bag was concatenated from multiple tensors, this address denotes which index belongs to which tensor
+            if balanced is True, address must be always passed to this function and contains at least one element
+        balanced (bool): if True and the bag was concatenated from multiple tensors, the returned features are drawn randomly with
+            equal number of features from each tensor. Defaults to False
+    '''
+    if balanced:
+        # The number of elements in address
+        n_address = len(address)
+        # If address is an empty dict
+        if n_address == 0:
+            raise ValueError("Address of features must contain at least 1 element.")
+        
+        # Ensure that address is in consistent format: {feats_index: [List of indexes]}
+        min_bag_size = np.inf
+        c_address = dict() # consistent_address
+        for k, v in address.items():
+            # v is a list of indexes
+            if isinstance(v, list):
+                # Each element must be an integer
+                if not all([isinstance(ele, int) for ele in v]):
+                    raise ValueError(f"Invalid value type of address. Expected intergers.")
+                c_address[k] = v
+                if len(v) < min_bag_size:
+                    min_bag_size = len(v)
+            # v is a tuple
+            elif isinstance(v, tuple):
+                # Each element must be an integer
+                if not all([isinstance(ele, int) for ele in v]):
+                    raise ValueError(f"Invalid value type of address. Expected intergers.")
+                # v has format (start_index, end_index) or (end_index, start_index)
+                if len(v) == 2:
+                    start_index, end_index = min(v), max(v)
+                    c_address[k] = np.arange(start_index, end_index + 1)
+                # else, treat v as a list
+                else:
+                    c_address[k] = list(v)
+                if len(v) < min_bag_size:
+                    min_bag_size = len(v)
+            # v is a number
+            elif isinstance(v, int):
+                c_address[k] = [v]
+                if 1 < min_bag_size:
+                    min_bag_size = 1
+            else:
+                raise ValueError(f"Invalid value type of address. Expected List, Tuple or Int, not {type(v)}")
+
+        # The number of index to be chosen
+        n_index = min(bag_size // n_address, min_bag_size)
+
+        # Get the indexes from each tensor with equal number, randomly
+        bag_idxs = []
+        for add in c_address.values():
+            bag_idxs.extend(np.random.choice(add, size=n_index, replace=False)) # replace = False means no repetition
+    
+    else:
+        bag_idxs = torch.randperm(bag.shape[0])[:bag_size]
+
     bag_samples = bag[bag_idxs]
 
     # zero-pad if we don't have enough samples
@@ -78,7 +140,7 @@ def _to_fixed_size_bag(
             torch.zeros(bag_size - bag_samples.shape[0], bag_samples.shape[1]),
         )
     )
-    return zero_padded, min(bag_size, len(bag))
+    return zero_padded, min(bag_size, len(bag), len(bag_samples))
 
 # -----------------------------------------------------------------------------
 
@@ -89,7 +151,8 @@ class BagDataset(Dataset):
         self,
         bags: Union[List[Path], List[np.ndarray], List[torch.Tensor], List[List[str]]],
         bag_size: Optional[int] = None,
-        preload: bool = False
+        preload: bool = False,
+        balanced: bool = False,
     ):
         """A dataset of bags of instances.
 
@@ -107,43 +170,66 @@ class BagDataset(Dataset):
                 containing more instances, a random sample of `bag_size`
                 instances will be drawn.  Smaller bags are padded with zeros.
                 If `bag_size` is None, all the samples will be used.
+            balanced (bool): if True and each bag inside bags was concatenated from multiple tensors, the returned features
+                are drawn randomly with equal number of features from each tensor. Defaults to False
 
         """
         super().__init__()
         self.bags = bags
         self.bag_size = bag_size
         self.preload = preload
+        self.balanced = balanced
 
         if self.preload:
+            # self.bags becomes a list of tuples
+            # each tuple is of format: (features (torch.Tensor), address(dict))
             self.bags = [self._load(i) for i in range(len(self.bags))]
 
     def __len__(self):
         return len(self.bags)
 
     def _load(self, index: int):
+        # Address is a dictionary mapping the index of feats originating from a different slide
+        address = dict()
         if isinstance(self.bags[index], str):
             feats = torch.load(self.bags[index]).to(torch.float32)
+            address = {0: (0, len(feats)-1)}
         elif isinstance(self.bags[index], np.ndarray):
             feats = torch.from_numpy(self.bags[index]).to(torch.float32)
+            address = {0: (0, len(feats)-1)}
         elif isinstance(self.bags[index], torch.Tensor):
             feats = self.bags[index]
+            address = {0: (0, len(feats)-1)}
         else:
-            feats = torch.cat([
-                torch.load(slide).to(torch.float32)
-                for slide in self.bags[index]
-            ])
-        return feats
+            # Load the first Tensor
+            feats = torch.load(self.bags[index][0]).to(torch.float32)
+            # Create the first address for Tensor at index 0
+            address = {0: (0, len(feats)-1)}
+            # iter_key is the key of the address dictionary
+            iter_key = 0
+            # For each Tensor from index 1
+            for slide in self.bags[index][1:]:
+                # Load Tensor
+                next_feats = torch.load(slide).to(torch.float32)
+                # Concat this Tensor to feats
+                feats = torch.cat([feats, next_feats])
+                # Create the address for this Tensor
+                iter_key += 1
+                start_index = address[iter_key-1][1] + 1
+                address[iter_key] = (start_index, start_index + len(next_feats) - 1)
+        
+        return feats, address
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
         # collect all the features
         if self.preload:
-            feats = self.bags[index]
+            feats, address = self.bags[index]
         else:
-            feats = self._load(index)
+            feats, address = self._load(index)
 
         # sample a subset, if required
         if self.bag_size:
-            return _to_fixed_size_bag(feats, bag_size=self.bag_size)
+            return _to_fixed_size_bag(feats, bag_size=self.bag_size, address=address, balanced=self.balanced)
         else:
             return feats, len(feats)
 
@@ -175,6 +261,13 @@ class MultiBagDataset(Dataset):
     `bag_size` is None, all the samples will be used.
     """
 
+    balanced: bool = False
+    """
+    balanced (bool): if True and each bag inside bags was concatenated from multiple tensors,
+    the returned features are drawn randomly with equal number of features from each tensor.
+    Defaults to False
+    """
+
     def __len__(self):
         return len(self.bags)
 
@@ -197,7 +290,7 @@ class MultiBagDataset(Dataset):
 
         # Sample a subset, if required
         if self.bag_size:
-            return [_to_fixed_size_bag(bag, bag_size=self.bag_size) for bag in loaded_bags]
+            return [_to_fixed_size_bag(bag, bag_size=self.bag_size, address={0: len(bag)}, balanced=self.balanced) for bag in loaded_bags]
         else:
             return [(bag, len(bag)) for bag in loaded_bags]
 
