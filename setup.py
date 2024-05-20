766775055974169b18fd626c37c3d5f3eb1abdd1
John Urbanik <johnurbanik@gmail.com>
2024-05-20 13:27:24 -0400

    loosen seaborn requirement

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setuptools.setup(
         'opencv-python-headless',
         'shapely',
         'umap-learn',
-        'seaborn<0.12',
+        'seaborn<=0.13',
         'pandas<2',
         'pyvips',
         'fpdf2',
