Fix mixed precision / feature extractor issue

slideflow/model/extractors/_factory_torch.py
@@ -1,7 +1,6 @@
 """Factory for building PyTorch feature extractors."""
 
 import inspect
-import numpy as np
 import slideflow as sf
 from typing import Tuple, Generator, Optional, TYPE_CHECKING
 from slideflow import errors
@@ -147,7 +146,8 @@ class TorchFeatureExtractor(BaseFeatureExtractor):
 
     def _process_output(self, output):
         """Process model output."""
-        return output
+        import torch
+        return output.to(torch.float32)
 
     def __call__(self, obj, **kwargs):
         """Generate features for a batch of images or a WSI."""

slideflow/model/extractors/virchow.py
@@ -80,7 +80,7 @@ class VirchowFeatures(TorchFeatureExtractor):
         class_token = output[:, 0]   # 1 x 1280
         patch_tokens = output[:, 1:] # 1 x 256 x 1280
         embedding = torch.cat([class_token, patch_tokens.mean(1)], dim=-1)  # 1 x 2560
-        return embedding
+        return embedding.to(torch.float32)
 
     def dump_config(self):
         """Return a dictionary of configuration parameters.